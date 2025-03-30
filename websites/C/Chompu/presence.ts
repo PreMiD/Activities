ea5a05989fd3afccb2bbab732290219411e3d35c
somboytiger <103308315+somboytiger@users.noreply.github.com>
2025-03-31 03:12:29 +0700

    update rebuild page

--- websites/C/Chompu/presence.ts
+++ websites/C/Chompu/presence.ts
@@ -9,43 +9,20 @@ enum ActivityAssets {
   Logo = 'https://cdn.rcd.gg/PreMiD/websites/C/Chompu/assets/logo.png',
 }
 
-enum Pages {
-  Home = '/',
-  Dashboard = '/dashboard',
-  Status = '/status',
-  Contact = '/contact',
-}
-
 const presenceData: PresenceData = {
   type: ActivityType.Listening,
   largeImageKey: ActivityAssets.Logo,
 }
 
 presence.on('UpdateData', async () => {
-  const base = document.location.pathname
 
-  if (/\/dashboard\/guild\/[^\d\n\r_\u2028\u2029]*[\d_].*\/music-room/i.test(base)) {
-    if (
-      document.querySelector<HTMLElement>('div.hidden.-player-status')
-      && document.querySelector<HTMLElement>('div.hidden.-player-status')
-        ?.textContent === 'true'
-    ) {
-      const author = document.querySelector<HTMLAnchorElement>(
-        'p.text-small.mt-1.text-foreground\\/80.-player-author',
-      )
-      const playing = document.querySelector<HTMLAnchorElement>(
-        'svg.-player-playing',
-      )
-      const timeEndPlayer = document.querySelector<HTMLElement>(
-        'p.text-small.text-foreground\\/50.-player-position-end',
-      )?.textContent
+    if (document.querySelector<HTMLElement>('[data-player-status=\'true\']')) {
+      const playing = document.querySelector<HTMLButtonElement>('button svg[data-player-pause="true"]');
       const [startPlayer, durationPlayer] = [
         presence.timestampFromFormat(
-          document.querySelector<HTMLElement>(
-            'p.text-small.-player-position-start',
-          )?.textContent ?? '',
+          document.querySelector<HTMLElement>('[data-player-position="true"]')?.textContent?.split(" / ")[0] ?? '',
         ),
-        presence.timestampFromFormat(timeEndPlayer ?? ''),
+        presence.timestampFromFormat(document.querySelector<HTMLElement>('[data-player-position="true"]')?.textContent?.split(" / ")[1] ?? ''),
       ]
       const [startTimestamp, endTimestamp] = presence.getTimestamps(
         startPlayer,
@@ -57,32 +34,17 @@ presence.on('UpdateData', async () => {
         endTimestamp,
       ]
 
-      presenceData.details = document.querySelector<HTMLAnchorElement>(
-        'h1.text-large.font-medium.-player-title',
-      )
-      presenceData.state = author
-      presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
-        '[data-label=\'guild-logo\']',
-      )?.src
+      presenceData.details = document.querySelector<HTMLElement>('[data-player-title]')?.textContent
+      presenceData.state = document.querySelector<HTMLElement>('[data-player-author]')?.textContent
+      presenceData.largeImageKey = document.querySelector<HTMLImageElement>('[data-player-image]')?.src
       presenceData.smallImageKey = playing ? Assets.Play : Assets.Pause
       presenceData.smallImageText = playing ? 'Playing' : 'Pause'
       presenceData.startTimestamp = startTimestamp
       presenceData.endTimestamp = endTimestamp
-      presenceData.buttons = [
-        {
-          label: `Join Player ${
-            document.querySelector<HTMLAnchorElement>(
-              '[data-label=\'player-requester\']',
-            )?.textContent
-          }`,
-          url: document.location.href,
-        },
-      ]
-
-      if (!playing)
-        delete presenceData.startTimestamp
-    }
-    else {
+      if (!playing) delete presenceData.startTimestamp
+      
+    } else {
+      console.log("no")
       presenceData.details = 'No song queue found'
       presenceData.state = 'In the server...'
       presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
@@ -93,43 +55,9 @@ presence.on('UpdateData', async () => {
         : ActivityAssets.Logo
       presenceData.smallImageText = 'Zzz'
       presenceData.startTimestamp = browsingTimestamp
-      presenceData.buttons = [
-        {
-          label: 'Join Player',
-          url: document.location.href,
-        },
-      ]
-      if (presenceData.endTimestamp)
-        delete presenceData.endTimestamp
+      if (presenceData.endTimestamp) delete presenceData.endTimestamp
     }
-  }
-  else {
-    presenceData.details = 'Idk'
-    presenceData.state = 'Browsing...'
-    presenceData.largeImageKey = ActivityAssets.Logo
-    presenceData.smallImageKey = Assets.Reading
-    presenceData.smallImageText = 'Zzz'
-    presenceData.startTimestamp = browsingTimestamp
-    if (presenceData.buttons)
-      delete presenceData.buttons
-    if (presenceData.endTimestamp)
-      delete presenceData.endTimestamp
-
-    switch (base) {
-      case Pages.Home:
-        presenceData.details = 'Home'
-        break
-      case Pages.Dashboard:
-        presenceData.details = 'Dashboard'
-        break
-      case Pages.Status:
-        presenceData.details = 'Status'
-        break
-      case Pages.Contact:
-        presenceData.details = 'Contact'
-        break
-    }
-  }
+  
 
   presence.setActivity(presenceData)
 })
