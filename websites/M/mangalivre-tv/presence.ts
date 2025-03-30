a48b78c68b87fe8e379ac0875f5ca448c92c805d
katsudouki <katsudouki@outlook.com.br>
2025-03-30 00:06:40 -0300

    🌟 feat: added https://mangalivre.tv/ presence
    Changelog: feature

---
+++ websites/M/mangalivre-tv/presence.ts
@@ -0,0 +1,65 @@
+const presence = new Presence({
+    clientId: "1355724922483114115" // Seu Client ID
+});
+
+const browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+    const presenceData: PresenceData = {
+        largeImageKey: "logo", // Key da logo do metadata.json
+        startTimestamp: browsingTimestamp
+    };
+
+    const { pathname, href } = window.location;
+    const pathParts = pathname.split('/').filter(part => part !== '');
+
+    try {
+        // Página inicial
+        if (pathname === "/") {
+            presenceData.details = "em: mangalivre.tv";
+            presenceData.state = "Escolhendo o que ler na página inicial";
+            presenceData.smallImageKey = "https://i.imgur.com/JEt3cEP.png";
+            presenceData.largeImageKey = "https://i.imgur.com/JEt3cEP.png";
+        }
+        
+        else if (pathParts[0] === "manga" && pathParts[2]?.startsWith("capitulo-")) {
+            const mangaName = pathParts[1]?.replace(/-/g, " ") || "Desconhecido";
+            const chapterNumber = pathParts[2]?.replace("capitulo-", "") || "0";
+
+            presenceData.details = `Lendo: ${mangaName}`;
+            presenceData.state = `Capítulo: ${chapterNumber}`;
+            presenceData.largeImageKey = "https://i.imgur.com/JEt3cEP.png";
+            presenceData.smallImageKey = "https://i.imgur.com/JEt3cEP.png"
+            presenceData.buttons = [
+                {
+                    label: `Capítulo ${chapterNumber}`,
+                    url: href
+                }
+            ];
+        }
+        // Página de mangá (ex: /manga/swordmasters-youngest-son/)
+        else if (pathParts[0] === "manga" && pathParts.length === 2) {
+            const mangaName = pathParts[1]?.replace(/-/g, " ") || "Desconhecido";
+            
+            presenceData.details = "Visualizando mangá";
+            presenceData.state = mangaName;
+            presenceData.largeImageKey = "https://i.imgur.com/JEt3cEP.png";
+            presenceData.smallImageKey = "https://i.imgur.com/JEt3cEP.png"
+        }
+        // Outras páginas
+        else {
+            presenceData.details = "Navegando no Mangá Livre";
+            presenceData.state = "Explorando conteúdo";
+            presenceData.largeImageKey = "https://i.imgur.com/JEt3cEP.png";
+            presenceData.smallImageKey = "https://i.imgur.com/JEt3cEP.png";
+        }
+
+        if (presenceData.details) {
+            presence.setActivity(presenceData);
+        } else {
+            presence.setActivity();
+        }
+    } catch (error) {
+        console.error("Erro no Presence:", error);
+    }
+});
