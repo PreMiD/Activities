a41fff819fd0549449e188978598d8c01c936fee
bookworm-coding <bookwormkkm@proton.me>
2025-04-04 22:30:28 +0900

    Linted code

--- websites/D/Dreamhack/presence.ts
+++ websites/D/Dreamhack/presence.ts
@@ -1,5 +1,3 @@
-import { Assets } from 'premid'
-
 const presence = new Presence({
   clientId: '1357670002727325809',
 })
@@ -25,85 +23,86 @@ const lvlImage: Record<number, string> = {
 }
 
 presence.on('UpdateData', async () => {
-  var rawpath = location.pathname
-  var path = rawpath.split('/')
+  const rawpath = location.pathname
+  const path = rawpath.split('/')
   const presenceData: PresenceData = {
     largeImageKey: ActivityAssets.Logo,
     startTimestamp: browsingTimestamp,
   }
   switch (path[1]) {
     case 'wargame':
-      if(rawpath.includes("challenges")) {
-        if(rawpath.includes("new")){
+      if (rawpath.includes('challenges')) {
+        if (rawpath.includes('new')) {
           presenceData.details = '워게임 문제 출제 중'
         }
-        else{
-          var tag = document.querySelector(
-            'div.tags'
+        else {
+          const tag = document.querySelector(
+            'div.tags',
           )!.textContent!.split(' ')[6]
-          presenceData.details = tag + ' 분야의 워게임 푸는 중'
+          presenceData.details = `${tag} 분야의 워게임 푸는 중`
           presenceData.state = document.querySelector(
-            '#challenge-info > h1'
+            '#challenge-info > h1',
           )!.textContent
-          var level = document.querySelector(
-            '#challenge-info > div.challenge-level > span'
+          const level = document.querySelector(
+            '#challenge-info > div.challenge-level > span',
           )!.textContent
-          if(level?.includes('LEVEL')){
-            var levelNum = parseInt(level.split(' ')[1] ?? '0')
-            presenceData.smallImageKey = lvlImage[levelNum+1]
-            presenceData.smallImageText = `레벨 ${levelNum+1}`
+          if (level?.includes('LEVEL')) {
+            const levelNum = Number.parseInt(level.split(' ')[1] ?? '0')
+            presenceData.smallImageKey = lvlImage[levelNum + 1]
+            presenceData.smallImageText = `레벨 ${levelNum + 1}`
           }
-          else{
-            if(level === "Unranked"){
+          else {
+            if (level === 'Unranked') {
               presenceData.smallImageKey = lvlImage[0]
-              presenceData.smallImageText = "Unranked"
+              presenceData.smallImageText = 'Unranked'
             }
-            else if(level === "Beginner"){
+            else if (level === 'Beginner') {
               presenceData.smallImageKey = lvlImage[1]
-              presenceData.smallImageText = "Beginner"
+              presenceData.smallImageText = 'Beginner'
             }
           }
         }
       }
-      else{
+      else {
         presenceData.details = '워게임 목록 보는 중'
       }
       break
     case 'lecture':
-      if(path[2] === 'roadmaps') {
-        if(path[3] === 'all'){
+      if (path[2] === 'roadmaps') {
+        if (path[3] === 'all') {
           presenceData.details = '로드맵 목록 보는 중'
         }
-        else{
+        else {
           presenceData.details = '로드맵 보는 중'
           presenceData.state = document.querySelector(
-              "#roadmap-title > div.content > div.title"
+            '#roadmap-title > div.content > div.title',
           )!.textContent
         }
       }
-      else if(path[2] === 'courses') {
+      else if (path[2] === 'courses') {
         presenceData.details = '강의 정보 보는 중'
         presenceData.state = document.querySelector(
-            "#course-title > div.content > div.course-title"
+          '#course-title > div.content > div.course-title',
         )!.textContent
       }
       break
     case 'ctf':
-      presenceData.details = "CTF 문제 풀이 중"
+      presenceData.details = 'CTF 문제 풀이 중'
       try {
         presenceData.state = document.querySelector(
-          "div.ctf-title > div.title"
+          'div.ctf-title > div.title',
         )!.textContent
-      } catch (error) {
+      }
+      catch {
         presenceData.state = document.querySelector(
-          "div.ctf-title"
+          'div.ctf-title',
         )!.textContent
       }
       break
     case 'users':
       presenceData.details = '유저 보는 중'
       presenceData.state = document.querySelector(
-          "div.profile-user > div.profile-user-profile > a > span"
+        'div.profile-user > div.profile-user-profile > a > span',
       )!.textContent
       break
     case 'board':
@@ -111,9 +110,9 @@ presence.on('UpdateData', async () => {
       break
     case 'forum':
       presenceData.details = '커뮤니티 보는 중'
-      if(rawpath.includes("posts") || rawpath.includes("qna")) {
+      if (rawpath.includes('posts') || rawpath.includes('qna')) {
         presenceData.state = document.querySelector(
-          "#__layout > div > main > div > div:nth-child(1) > div > div.header > div > div.left > div.title-wrapper > span"
+          '#__layout > div > main > div > div:nth-child(1) > div > div.header > div > div.left > div.title-wrapper > span',
         )!.textContent
       }
       break
