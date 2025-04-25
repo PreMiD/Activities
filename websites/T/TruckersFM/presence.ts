3d110be9d716aaefe9a9dac0c76003790e295b8c
Jack Dawson <77471397+jqcktalks@users.noreply.github.com>
2025-04-25 04:39:53 +0100

    feat(TruckersFM): remove redundant code, added listening status & album art support

--- websites/T/TruckersFM/presence.ts
+++ websites/T/TruckersFM/presence.ts
@@ -5,31 +5,17 @@ const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 presence.on('UpdateData', () => {
   const presenceData: PresenceData = {
-    largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png',
+    largeImageKey: (document.querySelector('.album-art') as HTMLImageElement)?.src || 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png',
     startTimestamp: browsingTimestamp,
+    type: 2,
   }
 
   presenceData.details = `${
     document.querySelector('.player-artist-text')?.textContent
   } - ${document.querySelector('.player-title-text')?.textContent}`
   presenceData.state = document.querySelector('.live-name')?.textContent ?? 'AutoDJ'
-
-  presenceData.buttons = [
-    {
-      label: 'Tune into TFM',
-      url: 'https://truckers.fm/listen',
-    },
-  ]
-
-  const spotifyUrl = document
-    .querySelector('.player-artist-text a')
-    ?.getAttribute('href')
-  if (spotifyUrl) {
-    presenceData.buttons.push({
-      label: 'Listen on Spotify',
-      url: spotifyUrl,
-    })
-  }
+  presenceData.smallImageKey = 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png'
+  presenceData.smallImageText = 'TruckersFM'
 
   presence.setActivity(presenceData)
 })
