40115d8552ef4e1498314b967121014342085956
Muhammad Rizki Syahputra <madriztra@gmail.com>
2025-05-06 04:00:48 +0700

    Add SkuyFun presence
    Signed-off-by: Muhammad Rizki Syahputra <madriztra@gmail.com>

---
+++ websites/S/skuyfun/presence.ts
@@ -0,0 +1,46 @@
+import { Assets, ActivityType } from 'premid';
+
+const presence = new Presence({
+  clientId: '1369034572859445399'
+});
+
+const browsingTimestamp = Math.floor(Date.now() / 1000);
+
+enum ActivityAssets { 
+  Logo = 'favicon',
+}
+
+presence.on('UpdateData', async () => {
+  const title = document.querySelector('title')?.textContent?.trim();
+  const episode = document.querySelector('h4 span:nth-child(2)')?.textContent;
+  const imgElement = document.querySelector('img.rounded-md') as HTMLImageElement;
+
+  let presenceData;
+
+  if (title && episode) {
+    presenceData = {
+      type: ActivityType.Watching,
+      details: `Watching: ${title}`,
+      state: `Episode: ${episode}`,
+      largeImageKey: imgElement,
+      smallImageKey: Assets.Play,
+      smallImageText: "You hovered me, and what now?",
+      startTimestamp: browsingTimestamp,
+      endTimestamp: browsingTimestamp + 1800,
+    };
+  } else {
+    presenceData = {
+      type: ActivityType.Watching,
+      details: "Browsing skuy.fun",
+      state: "Chillin' on the web",
+      largeImageKey: ActivityAssets.Logo,
+      smallImageKey: Assets.Play,
+      smallImageText: "Browse with me!",
+      startTimestamp: browsingTimestamp,
+      endTimestamp: browsingTimestamp + 1800,
+    };
+  }
+
+  console.log(presenceData);
+  presence.setActivity(presenceData);
+});
