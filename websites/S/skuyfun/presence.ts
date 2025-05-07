d8b18a4787f7ded002bb560ad6c1ff537c94fc44
Muhammad Rizki Syahputra <madriztra@gmail.com>
2025-05-07 16:56:13 +0700

    Update presence.ts
    Signed-off-by: Muhammad Rizki Syahputra <madriztra@gmail.com>

--- websites/S/skuyfun/presence.ts
+++ websites/S/skuyfun/presence.ts
@@ -1,36 +1,33 @@
-import { Assets, ActivityType } from 'premid';
+import { ActivityType, Assets } from 'premid'
 
 const presence = new Presence({
-  clientId: '1369034572859445399', 
-});
+  clientId: '1369034572859445399'
+})
 
-const browsingTimestamp = Math.floor(Date.now() / 1000);
+const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'favicon',
+  Logo = 'favicon'
 }
 
 presence.on('UpdateData', async () => {
-  
-  const title = document.querySelector('title')?.textContent?.trim();
+  const title = document.querySelector('title')?.textContent?.trim()
+  const episode = document.querySelector('h4 span:nth-child(2)')?.textContent
+  const imgElement = document.querySelector('img.rounded-md') as HTMLImageElement
 
-  const episode = document.querySelector('h4 span:nth-child(2)')?.textContent;
-
-  const imgElement = document.querySelector('img.rounded-md') as HTMLImageElement;
-
-  let presenceData;
+  let presenceData
 
   if (title && episode && imgElement) {
     presenceData = {
       type: ActivityType.Watching,
       details: `Watching: ${title}`,
       state: `Episode: ${episode}`,
-      largeImageKey: imgElement, 
+      largeImageKey: imgElement.src, // harus pakai src, bukan elemen DOM langsung
       smallImageKey: Assets.Play,
       smallImageText: 'You hovered me, and what now?',
       startTimestamp: browsingTimestamp,
-      endTimestamp: browsingTimestamp + 1800,
-    };
+      endTimestamp: browsingTimestamp + 1800
+    }
   } else {
     presenceData = {
       type: ActivityType.Watching,
@@ -40,11 +37,11 @@ presence.on('UpdateData', async () => {
       smallImageKey: Assets.Play,
       smallImageText: 'Browse with me!',
       startTimestamp: browsingTimestamp,
-      endTimestamp: browsingTimestamp + 1800,
-    };
+      endTimestamp: browsingTimestamp + 1800
+    }
   }
 
-  console.log(presenceData);
+  console.warn(presenceData)
 
-  presence.setActivity(presenceData);
-});
+  presence.setActivity(presenceData)
+})
