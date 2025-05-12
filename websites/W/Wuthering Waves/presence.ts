6137e70c60d46a393271e16581caf60ea9e7e225
Daniel Lau <daniel.lau@qmfremont.com>
2025-05-12 09:27:08 -0700

    chore: initialize activity

---
+++ websites/W/Wuthering Waves/presence.ts
@@ -0,0 +1,20 @@
+import { Assets } from 'premid'
+
+const presence = new Presence({
+  clientId: '503557087041683458',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = '',
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+    smallImageKey: Assets.Play,
+  }
+
+  presence.setActivity(presenceData)
+})
