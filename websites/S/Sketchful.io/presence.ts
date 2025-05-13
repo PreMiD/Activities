9bf7a0ff15f2739ff711d153be55c12a2a554098
Daniel Lau <daniel.lau@qmfremont.com>
2025-05-13 13:50:18 -0700

    fix: correct in-game detection logic

--- websites/S/Sketchful.io/presence.ts
+++ websites/S/Sketchful.io/presence.ts
@@ -25,7 +25,7 @@ presence.on('UpdateData', async () => {
     largeImageKey: 'https://i.imgur.com/ORnOKkS.png',
     startTimestamp: browsingTimestamp,
   }
-  const { pathname, href, hash } = document.location
+  const { pathname, href } = document.location
 
   if (pathname !== '/') {
     presenceData.details = 'Browsing'
@@ -35,69 +35,78 @@ presence.on('UpdateData', async () => {
   const activeMainMenu
     = document.querySelector<HTMLAnchorElement>('[href^=\'#menu\'].active')?.href
       ?? href
-  const activeTab = new URL(activeMainMenu).hash
+  const activeTab = new URL(activeMainMenu).hash.slice(1)
+  const gameContainer = document.querySelector<HTMLDivElement>('.game')
 
-  switch (activeTab) {
-    case 'menuPlay': {
-      presenceData.details = 'Preparing to Play'
-      break
-    }
-    case 'menuLobbies': {
-      presenceData.details = 'Browsing Lobbies'
-      break
-    }
-    case 'menuLogin': {
-      presenceData.details = 'Logging in'
-      break
-    }
-    default: {
-      if (hash) {
-        // in a game
-        const [useJoinButton, showDrawing] = await Promise.all([
-          presence.getSetting<boolean>('useJoinButton'),
-          presence.getSetting<boolean>('showDrawing'),
-        ])
-        const gameTimeLeft = Number(
-          document.querySelector('#gameClock')?.textContent ?? '0',
-        )
-        const gameRound = document.querySelector('#gameRound')
-        const gameContentContainer = document.querySelector('#gameSticky')
-        const answer = gameContentContainer?.querySelector('.answer')
-        const myCharacter = document
-          .querySelector('.gameAvatarName[style*=teal]')
-          ?.closest('li')
-        const drawingCharacter = document
-          .querySelector('.gameDrawing')
-          ?.closest('li')
+  if (gameContainer?.style.display === '') {
+    const [useJoinButton, showDrawing] = await Promise.all([
+      presence.getSetting<boolean>('useJoinButton'),
+      presence.getSetting<boolean>('showDrawing'),
+    ])
+    const gameTimeLeft = Number(
+      document.querySelector('#gameClock')?.textContent ?? '0',
+    )
+    const gameRound = document.querySelector('#gameRound')
+    const gameContentContainer = document.querySelector('#gameSticky')
+    const answer = gameContentContainer?.querySelector('.answer')
+    const myCharacter = document
+      .querySelector('.gameAvatarName[style*=teal]')
+      ?.closest('li')
+    const drawingCharacter = document
+      .querySelector('.gameDrawing')
+      ?.closest('li')
 
-        presenceData.type = ActivityType.Competing
-        presenceData.state = `Round ${gameRound?.textContent} | ${myCharacter?.querySelector('.gameAvatarRank')?.textContent} with ${myCharacter?.querySelector('.gameAvatarScore')?.textContent}`
-        if (gameTimeLeft > 0) {
-          delete presenceData.startTimestamp
-          presenceData.endTimestamp = getTimestamps(0, gameTimeLeft)[1]
-        }
+    presenceData.type = ActivityType.Competing
+    presenceData.state = `Round ${gameRound?.textContent} | ${myCharacter?.querySelector('.gameAvatarRank')?.textContent} with ${myCharacter?.querySelector('.gameAvatarScore')?.textContent}`
+    if (gameTimeLeft > 0) {
+      [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(0, gameTimeLeft)
+    }
 
-        if (myCharacter === drawingCharacter) {
-          if (gameContentContainer?.querySelector('b')) {
-            presenceData.details = 'Choosing a word'
-          }
-          else {
-            presenceData.details = 'Drawing'
-            presenceData.smallImageKey = Assets.Question
-            presenceData.smallImageText = answer
-          }
-        }
-        else {
-          presenceData.details = 'Guessing the word'
-        }
+    if (myCharacter === drawingCharacter) {
+      if (gameContentContainer?.querySelector('b')) {
+        presenceData.details = 'Choosing a word'
+      }
+      else {
+        presenceData.details = 'Drawing'
         if (showDrawing) {
           presenceData.largeImageKey = getCurrentImage()
         }
-        if (useJoinButton) {
-          presenceData.buttons = [{ label: 'Join Game', url: href }]
-        }
       }
-      else {
+    }
+    else if (answer) {
+      presenceData.details = 'Guessing the word'
+      presenceData.smallImageKey = Assets.Question
+      presenceData.smallImageText = answer
+      if (showDrawing) {
+        presenceData.largeImageKey = getCurrentImage()
+      }
+    }
+    else {
+      presenceData.details = 'Waiting for players'
+    }
+
+    if (useJoinButton) {
+      const url = document.querySelector<HTMLInputElement>('#roomInfoLink')?.value
+      if (url) {
+        presenceData.buttons = [{ label: 'Join Game', url: `https://${url}` }]
+      }
+    }
+  }
+  else {
+    switch (activeTab) {
+      case 'menuPlay': {
+        presenceData.details = 'Preparing to Play'
+        break
+      }
+      case 'menuLobbies': {
+        presenceData.details = 'Browsing Lobbies'
+        break
+      }
+      case 'menuLogin': {
+        presenceData.details = 'Logging in'
+        break
+      }
+      default: {
         presenceData.details = 'Browsing'
       }
     }
