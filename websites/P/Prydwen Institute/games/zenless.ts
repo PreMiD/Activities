fcaafedcc6e4e7f0575ae46060a1db8707ba5321
Daniel Lau <theusafyt@gmail.com>
2025-05-14 18:02:39 -0700

    feat(Zenless Zone Zero): add game details

--- websites/P/Prydwen Institute/games/zenless.ts
+++ websites/P/Prydwen Institute/games/zenless.ts
@@ -1,4 +1,4 @@
-import { addButton, registerSlideshowKey, slideshow } from '../util.js'
+import { addButton, registerSlideshowKey, slideshow, useActive } from '../util.js'
 
 export function apply(presenceData: PresenceData, pathList: string[]) {
   switch (pathList[0]) {
@@ -14,20 +14,72 @@ export function apply(presenceData: PresenceData, pathList: string[]) {
       break
     }
     case 'tier-list': {
-      const selection = document.querySelector('.tier-list-switcher .selected')
       const characters = document.querySelectorAll('.avatar-card')
       presenceData.details = 'Browsing Tier List'
-      presenceData.state = selection
-      if (registerSlideshowKey(`zenless-list-${selection?.textContent}-${characters.length}`)) {
+      if (registerSlideshowKey(`zenless-list-${characters.length}`)) {
         for (const character of characters) {
           const link = character.querySelector('a')
+          const image = character.querySelector<HTMLImageElement>('[data-main-image]')
           const data: PresenceData = {
             ...presenceData,
-            smallImageKey: character.querySelector<HTMLImageElement>('[data-main-image]'),
-            smallImageText: `${character.closest('.custom-tier')?.querySelector('.tier-rating')?.textContent} - ${character.querySelector('.emp-name')}`,
+            smallImageKey: image,
+            smallImageText: `${character.closest('.custom-tier')?.querySelector('.tier-rating')?.textContent} - ${image?.alt}`,
           }
-          addButton(data, { label: 'View Character', url: link })
+          addButton(data, { label: 'View Agent', url: link })
           slideshow.addSlide(link?.href ?? '', data, 5000)
+        }
+      }
+      return true
+    }
+    case 'shiyu-defense': {
+      presenceData.details = 'Reading About Shiyu Defense Analytics'
+      break
+    }
+    case 'deadly-assault': {
+      presenceData.details = 'Reading About Deadly Assault Analytics'
+      break
+    }
+    case 'bangboo': {
+      const { active } = useActive(document.querySelector('.bangboo-simplified-view'))
+      if (active) {
+        presenceData.details = 'Viewing a Bangboo'
+        presenceData.state = active.querySelector('.name')
+        presenceData.smallImageKey = active.querySelector<HTMLImageElement>('[data-main-image]')
+        presenceData.smallImageText = active.querySelector('.nav-link.active')
+      }
+      else {
+        presenceData.details = 'Browsing Bangboo'
+      }
+      break
+    }
+    case 'w-engines': {
+      presenceData.details = 'Browsing W-Engines'
+      const engines = document.querySelectorAll('.zzz-engine')
+      if (registerSlideshowKey(`zenless-w-engines-${engines.length}`)) {
+        for (const engine of engines) {
+          const name = engine.querySelector('h5')
+          const data: PresenceData = {
+            ...presenceData,
+            smallImageKey: engine.querySelector<HTMLImageElement>('[data-main-image]'),
+            smallImageText: name,
+          }
+          slideshow.addSlide(name?.textContent ?? '', data, 5000)
+        }
+      }
+      return true
+    }
+    case 'disk-drives': {
+      presenceData.details = 'Browsing Drive Disks'
+      const engines = document.querySelectorAll('.zzz-disk-set')
+      if (registerSlideshowKey(`zenless-w-engines-${engines.length}`)) {
+        for (const engine of engines) {
+          const name = engine.querySelector('h5')
+          const data: PresenceData = {
+            ...presenceData,
+            smallImageKey: engine.querySelector<HTMLImageElement>('[data-main-image]'),
+            smallImageText: name,
+          }
+          slideshow.addSlide(name?.textContent ?? '', data, 5000)
         }
       }
       return true
