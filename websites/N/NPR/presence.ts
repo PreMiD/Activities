3b1b4913675895ad3435524bdc48b73cc380920a
Daniel Lau <daniel.lau@qmfremont.com>
2025-05-19 15:17:32 -0700

    feat(NPR): initialize activity

---
+++ websites/N/NPR/presence.ts
@@ -0,0 +1,17 @@
+const presence = new Presence({
+  clientId: '503557087041683458',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/BdabueB.png',
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+  }
+
+  presence.setActivity(presenceData)
+})
