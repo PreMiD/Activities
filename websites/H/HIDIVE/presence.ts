3706b99de4fc9da004593ce6b98d285e86902557
xDoritos <agusssd0@gmail.com>
2025-06-04 05:47:02 +0000

    feat(HIDIVE) add activity V6 Reviewed 2

--- websites/H/HIDIVE/presence.ts
+++ websites/H/HIDIVE/presence.ts
@@ -268,7 +268,7 @@ presence.on('UpdateData', async () => {
     const seasonIdFromUrl = pathname.split('/season/')[1]?.split('/')[0] || ''
 
     if (seasonIdFromUrl && seasonIdFromUrl !== cachedSeason.seasonId) {
-     await updateSeasonCache(seasonIdFromUrl)
+      await updateSeasonCache(seasonIdFromUrl)
     }
 
     const animeTitle = cachedSeason.title || 'Unknown Title'
