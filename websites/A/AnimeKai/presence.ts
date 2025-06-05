f78dd2863a81ddb131c900323989afc5f43f7d99
DisguiseGuy <mastergamer15704@gmail.com>
2025-06-05 22:21:49 +0600

    Github scan fix 2

--- websites/A/AnimeKai/presence.ts
+++ websites/A/AnimeKai/presence.ts
@@ -195,7 +195,8 @@ presence.on('UpdateData', async () => {
         const type = new URLSearchParams(window.location.search).get('type')
         if (type === 'community') {
           presenceData.details = 'Looking at Community Notifications'
-        } else {
+        } 
+        else {
           presenceData.details = 'Looking at Anime Notifications'
         }
         presenceData.smallImageKey = Assets.Reading
