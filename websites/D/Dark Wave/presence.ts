2fd78e678cf1614f26716678d18682288fed3f70
ForgeOff <forgeoffdev@gmail.com>
2025-06-15 05:38:33 +0200

    bugfix(DW-2): eslint

--- websites/D/Dark Wave/presence.ts
+++ websites/D/Dark Wave/presence.ts
@@ -1,52 +1,53 @@
-import {ActivityType, Assets, getTimestamps} from "../../../premid/src/index.js";
+import { ActivityType, Assets, getTimestamps } from '../../../premid/src/index.js'
 
 const presence = new Presence({
-    clientId: "1383644853589643385"
-});
-const titleInfo: any = {};
-presence.on("UpdateData", async () => {
-    const data: PresenceData = {
-        type: ActivityType.Watching,
-        name: 'DarkWave - Looking..',
-        buttons: [
-            {
-                label: 'Open DarkWave',
-                url: `https://dark-wave.fr`,
-            }
-        ],
-        largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/D/Dark%20Wave/assets/logo.png'
-    };
+  clientId: '1383644853589643385',
+})
+const titleInfo: any = { }
+presence.on('UpdateData', async () => {
+  const data: PresenceData = {
+    type: ActivityType.Watching,
+    name: 'DarkWave - Looking..',
+    buttons: [
+      {
+        label: 'Open DarkWave',
+        url: `https://dark-wave.fr`,
+      },
+    ],
+    largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/D/Dark%20Wave/assets/logo.png',
+  }
 
-    const video = document.querySelector("video") as HTMLVideoElement;
-    if (video) {
-        if (!video.paused) {
-            const timestamps = getTimestamps(video.currentTime, video.duration);
-            data['startTimestamp'] = timestamps[0];
-            data['endTimestamp'] = timestamps[1];
-        }else {
-            data['smallImageKey'] = Assets.Pause;
-            data['smallImageText'] = 'Paused';
-        }
-
-        const videoName = document.querySelector('#app > div > div > div > div > div.w-full.absolute.bottom-0.right-0.left-0 > div.relative.z-20.p-grid-xs.pt-0 > div > div:nth-child(1) > div > div > small')?.innerHTML;
-        const videoTitle = document.querySelector('#app > div > div > div > div > div.w-full.absolute.bottom-0.right-0.left-0 > div.relative.z-20.p-grid-xs.pt-0 > div > div:nth-child(1) > div > div > h1')?.innerHTML;
-
-        if (videoTitle && titleInfo.title !== videoTitle) {
-            titleInfo.title = videoTitle;
-            titleInfo.name = videoName;
-        }
-        if (titleInfo?.title) {
-            data['name'] = titleInfo.name ?? titleInfo.title;
-            data['details'] = titleInfo.title;
-
-            if (titleInfo.name) {
-                data['state'] = titleInfo.name;
-            }
-        }
+  const video = document.querySelector('video') as HTMLVideoElement
+  if (video) {
+    if (!video.paused) {
+      const timestamps = getTimestamps(video.currentTime, video.duration)
+      data.startTimestamp = timestamps[0]
+      data.endTimestamp = timestamps[1]
     }
-    data.buttons?.push({
-        label: `Watch ${titleInfo.name ?? titleInfo.title}`,
-        url: document.location.href
-    })
-    presence.setActivity(data);
-});
+    else {
+      data.smallImageKey = Assets.Pause
+      data.smallImageText = 'Paused'
+    }
+
+    const videoName = document.querySelector('#app > div > div > div > div > div.w-full.absolute.bottom-0.right-0.left-0 > div.relative.z-20.p-grid-xs.pt-0 > div > div:nth-child(1) > div > div > small')?.innerHTML
+    const videoTitle = document.querySelector('#app > div > div > div > div > div.w-full.absolute.bottom-0.right-0.left-0 > div.relative.z-20.p-grid-xs.pt-0 > div > div:nth-child(1) > div > div > h1')?.innerHTML
+
+    if (videoTitle && titleInfo.title !== videoTitle) {
+      titleInfo.title = videoTitle
+      titleInfo.name = videoName
+    }
+    if (titleInfo?.title) {
+      data.name = titleInfo.name ?? titleInfo.title
+      data.details = titleInfo.title
+
+      if (titleInfo.name) {
+        data.state = titleInfo.name
+      }
+    }
+  }
+  data.buttons?.push({
+    label: `Watch ${titleInfo.name ?? titleInfo.title}`,
+    url: document.location.href,
+  })
+  presence.setActivity(data)
+})
