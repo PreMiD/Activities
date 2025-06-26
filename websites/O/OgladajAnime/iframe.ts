fe4c889de2059e2dacfadc88ef82e480b496a651
HAHOOS <hahoos@hahoos.pl>
2025-06-26 15:47:35 +0200

    fix(OgladajAnime): disallow youtube from iframe to avoid trailers on tooltips changing the timestamp

--- websites/O/OgladajAnime/iframe.ts
+++ websites/O/OgladajAnime/iframe.ts
@@ -1,12 +1,14 @@
 const iframe = new iFrame()
 
 setInterval(() => {
-  const video = document.querySelector('video')
-  if (video != null && !Number.isNaN(video.duration)) {
-    iframe.send({
-      currTime: video.currentTime,
-      duration: video.duration,
-      paused: video.paused,
-    })
+  if (!document.location.hostname.includes('youtube.com') && !document.location.hostname.includes('youtu.be')) {
+    const video = document.querySelector('video')
+    if (video != null && !Number.isNaN(video.duration)) {
+      iframe.send({
+        currTime: video.currentTime,
+        duration: video.duration,
+        paused: video.paused,
+      })
+    }
   }
 }, 500)
