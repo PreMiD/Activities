6f70117425df8f1af1de993cb895a603e50916f0
HAHOOS <hahoos@hahoos.pl>
2025-07-02 21:33:45 +0200

    feat(OgladajAnime): use iFrameRegExp to disallow youtube iFrames instead of checking in the code

--- websites/O/OgladajAnime/iframe.ts
+++ websites/O/OgladajAnime/iframe.ts
@@ -1,14 +1,12 @@
 const iframe = new iFrame()
 
 setInterval(() => {
-  if (!document.location.hostname.includes('youtube.com') && !document.location.hostname.includes('youtu.be')) {
-    const video = document.querySelector('video')
-    if (video != null && !Number.isNaN(video.duration)) {
-      iframe.send({
-        currTime: video.currentTime,
-        duration: video.duration,
-        paused: video.paused,
-      })
-    }
+  const video = document.querySelector('video')
+  if (video != null && !Number.isNaN(video.duration)) {
+    iframe.send({
+      currTime: video.currentTime,
+      duration: video.duration,
+      paused: video.paused,
+    })
   }
 }, 500)
