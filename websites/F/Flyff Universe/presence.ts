4d2e0730f64afad0098a1f9cec6204b214cd7a55
Khurram Ali <khurramcali@gmail.com>
2025-07-03 01:08:28 -0400

    feat(Flyff Universe): update activity

---
+++ websites/F/Flyff Universe/presence.ts
@@ -0,0 +1,34 @@
+import { Assets } from 'premid'
+
+const presence = new Presence({
+  clientId: '1390190087634944060',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/WTLdzZL.png',
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+  }
+
+  const path = document.location.pathname
+
+  if (path === '/') {
+    presenceData.state = 'Home'
+  }
+  else if (path.includes('/news')) {
+    presenceData.state = 'Posts'
+  }
+  else if (path.includes('/play')) {
+    presenceData.state = 'Flyff Universe'
+  }
+  else {
+    presenceData.state = 'Browsing'
+  }
+
+  presence.setActivity(presenceData)
+})
