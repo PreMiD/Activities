76c80c871b4a65a2721e4854e232801a7c2949d6
Kagurazaka-Nana <nightsky_chaser@outlook.com>
2025-07-20 22:10:10 +0800

    feat(Gronkh.tv): add presences when watching streams, includes timestamps, title and video ID

--- websites/G/Gronkh.tv/presence.ts
+++ websites/G/Gronkh.tv/presence.ts
@@ -1,4 +1,4 @@
-import { Assets } from 'premid'
+import { ActivityType, Assets } from 'premid'
 
 const presence = new Presence({
   clientId: '1396486294779068416',
@@ -12,9 +12,40 @@ enum ActivityAssets { // Other default assets can be found at index.d.ts
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
     largeImageKey: ActivityAssets.Logo,
+    type: ActivityType.Watching,
     startTimestamp: browsingTimestamp,
     // smallImageKey: Assets.Play,
+    endTimestamp: undefined,
+    details: 'Viewing Gronkh.tv',
   }
 
+  const curURL = document.location.href
+
+  if (curURL.includes('streams')) {
+    const el = document.querySelector('video');
+    if (el instanceof HTMLVideoElement) {
+      const timestamps = presence.getTimestamps(el.currentTime, el.duration);
+      const isPlaying = !el.paused && !el.ended && el.readyState > 2;
+      if (isPlaying) {
+        presenceData.smallImageKey = Assets.Play
+      }
+      else {
+        presenceData.smallImageKey = Assets.Pause
+      }
+      presenceData.startTimestamp = timestamps[0];
+      presenceData.endTimestamp = timestamps[1];
+    }
+    const match = curURL.match(/\/streams\/(\d+)/)
+    const videoId = match?.[1] || null
+    const videoTitle = document.querySelector('.g-video-meta-title')?.textContent?.trim()
+    const detailsText = `${videoTitle} · #${videoId}`
+    presenceData.details = detailsText;
+    presenceData.buttons = [
+      {
+        label: 'Watch Together',
+        url: curURL
+      }
+    ]
+  }
   presence.setActivity(presenceData)
 })
