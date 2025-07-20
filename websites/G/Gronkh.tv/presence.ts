8459e40d63c689c27ad1f512efcba2a2301911a5
Kagurazaka-Nana <nightsky_chaser@outlook.com>
2025-07-20 22:23:36 +0800

    chore: format the codes

--- websites/G/Gronkh.tv/presence.ts
+++ websites/G/Gronkh.tv/presence.ts
@@ -1,4 +1,5 @@
-import { ActivityType, Assets } from 'premid'
+import { ActivityType, Assets, getTimestamps } from 'premid'
+
 
 const presence = new Presence({
   clientId: '1396486294779068416',
@@ -22,29 +23,29 @@ presence.on('UpdateData', async () => {
   const curURL = document.location.href
 
   if (curURL.includes('streams')) {
-    const el = document.querySelector('video');
+    const el = document.querySelector('video')
     if (el instanceof HTMLVideoElement) {
-      const timestamps = presence.getTimestamps(el.currentTime, el.duration);
-      const isPlaying = !el.paused && !el.ended && el.readyState > 2;
+      const timestamps = getTimestamps(el.currentTime, el.duration)
+      const isPlaying = !el.paused && !el.ended && el.readyState > 2
       if (isPlaying) {
         presenceData.smallImageKey = Assets.Play
       }
       else {
         presenceData.smallImageKey = Assets.Pause
       }
-      presenceData.startTimestamp = timestamps[0];
-      presenceData.endTimestamp = timestamps[1];
+      presenceData.startTimestamp = timestamps[0]
+      presenceData.endTimestamp = timestamps[1]
     }
     const match = curURL.match(/\/streams\/(\d+)/)
     const videoId = match?.[1] || null
     const videoTitle = document.querySelector('.g-video-meta-title')?.textContent?.trim()
     const detailsText = `${videoTitle} · #${videoId}`
-    presenceData.details = detailsText;
+    presenceData.details = detailsText
     presenceData.buttons = [
       {
         label: 'Watch Together',
-        url: curURL
-      }
+        url: curURL,
+      },
     ]
   }
   presence.setActivity(presenceData)
