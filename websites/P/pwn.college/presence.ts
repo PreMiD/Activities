63ff5c20a121b88e5fff3904d3562b0cc44897de
rekka69 <rekka69@proton.me>
2025-07-22 08:41:56 +0300

    Added pwn.college presence

---
+++ websites/P/pwn.college/presence.ts
@@ -0,0 +1,81 @@
+import { Assets } from 'premid'
+
+const presence = new Presence({
+  clientId: '1397042912386224280',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+const { pathname } = document.location
+
+const beltsURLs: Record<string, string> = {
+  orange: 'https://cdn.imgchest.com/files/739cxk66w87.png',
+  yellow: 'https://cdn.imgchest.com/files/y2pck5oolk7.png',
+  green: 'https://cdn.imgchest.com/files/4jdcv6jj8g4.png',
+  blue: 'https://cdn.imgchest.com/files/yq9c3veema4.png',
+  white: 'https://cdn.imgchest.com/files/7lxcpxddle7.png'
+}
+
+const beltsArray: string[] = Object?.keys(beltsURLs)
+let currentBelt = "white"
+let nextBelt = 'orange'
+
+const messages = {
+  // mastering: `🔍 Mastering: ${module} technique`
+}
+
+const utils = {
+  stripPercent: (percentage: string = "0%") => +percentage.replace("%", ""),
+ }
+
+
+enum ActivityAssets { // Other default assets can be found at index.d.ts
+  Logo = 'https://cdn.imgchest.com/files/yd5cemrrpz4.png',
+  thumbnail = 'https://cdn.imgchest.com/files/yrgcn9zz3b4.png', 
+}
+
+presence.on('UpdateData', async () => {
+  console.log(beltsURLs[currentBelt])
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+    smallImageKey: beltsURLs[currentBelt], 
+  }
+  presenceData.details = currentBelt != 'blue' ? `🥋 Grinding for the ${nextBelt} belt!` : `☯️ You're now a sensei; help others cross the path!` 
+      
+  if(pathname == '/') {
+    
+    const cards = document.querySelectorAll("html body main div.container ul.card-list")[1]
+    const beltsCards = cards?.querySelectorAll(".progress-bar")
+    if(cards && beltsCards) {
+      beltsCards?.forEach((element, i) => {
+        const bar = element as HTMLElement;
+        const progress = utils.stripPercent(bar?.style?.width)
+        if(progress == 100) {
+          currentBelt = beltsArray[i]!
+          nextBelt = beltsArray[++i]!
+        }
+      });
+  
+    } 
+  }
+
+  if(pathname.split('/').length == 3) {
+    const moduleName = document.querySelector("h1.brand-white")?.textContent 
+    presenceData.state = moduleName ? `📜 Meditating on module selection: ${moduleName}` : '' 
+  }
+
+  if(pathname.split('/').length == 4) {
+    const moduleName = document.querySelector("h1.brand-white")?.textContent 
+    presenceData.state = moduleName ? `⚔️ Mastering ${moduleName}...` : '' 
+  }
+
+  if(pathname.includes('/hacker')) {
+    presenceData.details = "🎋 Looking at their profile"
+    presenceData.state = `🌸 Reflecting on their journey...`
+  }
+  
+  if(pathname.includes('/settings')) {
+    presenceData.details = "⚙️ Tweaking the experience..."
+    presenceData.state = ''
+  }
+  presence.setActivity(presenceData)
+})
