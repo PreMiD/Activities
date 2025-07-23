5826aae0d5b0e4231f2e55b37a570c9507b5ee19
HAHOOS <hahoos@hahoos.dev>
2025-07-23 17:17:53 +0200

    fix(Sflix): fix if statement checking only for null
    Signed-off-by: HAHOOS <hahoos@hahoos.dev>

--- websites/S/Sflix/iframe.ts
+++ websites/S/Sflix/iframe.ts
@@ -2,7 +2,7 @@ const iframe = new iFrame()
 
 iframe.on('UpdateData', () => {
   const video = document.querySelector('video')
-  if (video != null && !Number.isNaN(video.duration)) {
+  if (video && !Number.isNaN(video.duration)) {
     iframe.send({
       currTime: video.currentTime,
       duration: video.duration,
