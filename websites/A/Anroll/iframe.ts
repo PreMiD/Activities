5774bced8f3bbab2adbb8eec8686f1d164f3dc38
Nest <80919913+nestzin@users.noreply.github.com>
2025-07-27 21:18:28 -0300

    Update iframe.ts
    Signed-off-by: Nest <80919913+nestzin@users.noreply.github.com>

--- websites/A/Anroll/iframe.ts
+++ websites/A/Anroll/iframe.ts
@@ -1,56 +1,20 @@
-interface VideoData {
-  currentTime: number;
-  duration: number;
-  paused: boolean;
-}
+const iframe = new iFrame()
 
-class IFrameHandler {
-  private iframe = new iFrame();
-  private lastVideoState: VideoData | null = null;
+setInterval(() => {
+	const video = document.querySelector<HTMLVideoElement>('#player0') ?? 
+		document.querySelector<HTMLVideoElement>('#player_html5_api') ?? 
+		document.querySelector<HTMLVideoElement>('video')
 
-  constructor() {
-    this.init();
-  }
-
-  private init(): void {
-    setInterval(() => this.checkVideo(), 500);
-  }
-
-  private getVideoElement(): HTMLVideoElement | null {
-    return document.querySelector('#player0') || 
-           document.querySelector('#player_html5_api') || 
-           document.querySelector('video');
-  }
-
-  private shouldUpdate(currentState: VideoData): boolean {
-    if (!this.lastVideoState) return true;
-    
-    return (
-      Math.abs(currentState.currentTime - this.lastVideoState.currentTime) > 1 ||
-      currentState.paused !== this.lastVideoState.paused
-    );
-  }
-
-  private checkVideo(): void {
-    try {
-      const video = this.getVideoElement();
-      
-      if (video && !Number.isNaN(video.duration)) {
-        const currentState: VideoData = {
-          currentTime: video.currentTime,
-          duration: video.duration,
-          paused: video.paused
-        };
-
-        if (this.shouldUpdate(currentState)) {
-          this.iframe.send({ videoData: currentState });
-          this.lastVideoState = currentState;
-        }
-      }
-    } catch (error) {
-      console.error('Video check error:', error);
-    }
-  }
-}
+	if (video && !Number.isNaN(video.duration)) {
+		iframe.send({
+			iFrameVideoData: {
+				iFrameVideo: true,
+				currTime: video.currentTime,
+				dur: video.duration,
+				paused: video.paused,
+			},
+		})
+	}
+}, 100)
 
 new IFrameHandler();
