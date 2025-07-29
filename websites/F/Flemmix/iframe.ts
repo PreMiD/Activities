a74c651fb395054ca7c22e1824af8a124c9c3b74
alan <75907296+alan7383@users.noreply.github.com>
2025-07-30 00:18:32 +0200

    feat(Flemmix): add presence (2/3 & 3/3)
    Signed-off-by: alan <75907296+alan7383@users.noreply.github.com>

---
+++ websites/F/Flemmix/iframe.ts
@@ -0,0 +1,15 @@
+declare const iFrame: any;
+
+const iFrameHandler = new iFrame()
+
+iFrameHandler.on('UpdateData', async () => {
+  const video = document.querySelector<HTMLVideoElement>('video')
+  if (video && !Number.isNaN(video.duration)) {
+    // On utilise notre nouvelle variable ici
+    iFrameHandler.send({
+      currentTime: video.currentTime,
+      duration: video.duration,
+      paused: video.paused,
+    })
+  }
+})
