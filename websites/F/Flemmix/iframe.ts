231fc8ba32b69da885e69c5f604a618f7238f8b4
alan <75907296+alan7383@users.noreply.github.com>
2025-08-01 16:05:19 +0200

    refactor(Flemmix): use global types for iFrame
    Signed-off-by: alan <75907296+alan7383@users.noreply.github.com>

--- websites/F/Flemmix/iframe.ts
+++ websites/F/Flemmix/iframe.ts
@@ -1,11 +1,8 @@
-declare const iFrame: any
-
 const iFrameHandler = new iFrame()
 
 iFrameHandler.on('UpdateData', async () => {
   const video = document.querySelector<HTMLVideoElement>('video')
   if (video && !Number.isNaN(video.duration)) {
-    // On utilise notre nouvelle variable ici
     iFrameHandler.send({
       currentTime: video.currentTime,
       duration: video.duration,
