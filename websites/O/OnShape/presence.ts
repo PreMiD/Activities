ab6a6636158d897f78c3a912dc5b20848dbbba9c
Guhszvv <vruum962@gmail.com>
2025-08-09 16:02:30 -0300

    fix(OnShape): fix extra semicolon and singlequote

--- websites/O/OnShape/presence.ts
+++ websites/O/OnShape/presence.ts
@@ -1,8 +1,8 @@
 const presence = new Presence({
   clientId: '1403785669171740814',
-});
+})
 
-const browsingTimestamp = Math.floor(Date.now() / 1000);
+const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
   // Other default assets can be found at index.d.ts
@@ -10,31 +10,32 @@ enum ActivityAssets {
 }
 
 function getCleanOnshapeTitle(): string {
-  const rawTitle = document.title;
-  if (rawTitle.includes("Documents")) {
-    return "Searching...";
+  const rawTitle = document.title
+  if (rawTitle.includes('Documents')) {
+    return 'Searching...'
   }
-  return rawTitle.replace(" - Onshape", "").trim();
+  return rawTitle.replace(' - Onshape', '').trim()
 }
 
 function getOnshapeStatus(): string {
-  const status = document.title;
-  if (status.includes("Documents")) {
-    return "";
-  } else {
-    return "Editing...";
+  const status = document.title
+  if (status.includes('Documents')) {
+    return ''
+  } 
+  else {
+    return 'Editing...';
   }
 }
 
 presence.on('UpdateData', async () => {
-  const title = getCleanOnshapeTitle();
-  const state = getOnshapeStatus();
+  const title = getCleanOnshapeTitle()
+  const state = getOnshapeStatus()
   const presenceData: PresenceData = {
     largeImageKey: ActivityAssets.Logo,
     startTimestamp: browsingTimestamp,
     details: `${title}`,
-    state: `${state}`,
-  };
+    state: `${state}`
+  }
 
-  presence.setActivity(presenceData);
-});
+  presence.setActivity(presenceData)
+})
