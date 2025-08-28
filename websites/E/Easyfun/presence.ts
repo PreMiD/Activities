efa991845ce3ce8df2df9efc6de5cba3bc2e914c
Al3ss1Am0d <131905914+Al3ss1Am0d@users.noreply.github.com>
2025-08-28 09:57:22 +0200

    Update presence.ts
    Signed-off-by: Al3ss1Am0d <131905914+Al3ss1Am0d@users.noreply.github.com>

--- websites/E/Easyfun/presence.ts
+++ websites/E/Easyfun/presence.ts
@@ -1,22 +1,22 @@
 const presence = new Presence({
-  clientId: '1408054441252491326'
+  clientId: '1408054441252491326',
 })
 
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://geographical-lime-9pydlktowb.edgeone.app/263cef731b755103d3a010a8941458d7%20(1).png'
+  Logo = 'https://i.postimg.cc/nzGR3SrC/263cef731b755103d3a010a8941458d7-1.png', // imagem válida 512x512
 }
 
 presence.on('UpdateData', async () => {
   const { pathname, href } = document.location
 
-  const match = pathname.match(/\/cloud-games\/([a-z0-9-]+)\.html/i)
+  const match = pathname.match(/\/cloud-games\/[a-z0-9-]+\.html/i) // grupo de captura removido
   let gameName = null
 
-  if (match && match[1]) {
-    gameName = match[1]
-      .replace(/(-cloud.*|\.html)$/i, '')
+  if (match) {
+    const rawName = pathname.split('/').pop()?.replace(/(-cloud.*|\.html)$/i, '') ?? ''
+    gameName = rawName
       .split('-')
       .map(word =>
         /^\d+$/.test(word)
@@ -34,7 +34,7 @@ presence.on('UpdateData', async () => {
     details: gameName ? `Playing ${gameName}` : 'Exploring EasyFun',
     state: gameName ? 'Cloud Gaming' : 'Browsing on site',
     startTimestamp: browsingTimestamp,
-    buttons: gameName ? [{ label: 'Play now', url: href }] : undefined
+    buttons: gameName ? [{ label: 'Play now', url: href }] : undefined,
   }
 
   presence.setActivity(presenceData)
