f3721edb5dce005e4c55782e6c7bc09cd6cdfb61
Bruno Desktop <carecsnay1@gmail.com>
2025-08-30 12:45:00 -0300

    fix: logo size to 512px

--- websites/F/FSC/presence.ts
+++ websites/F/FSC/presence.ts
@@ -8,7 +8,7 @@ const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    largeImageKey: 'https://i.imgur.com/mFF5hMO.png',
+    largeImageKey: 'https://i.imgur.com/6gzGsDp.png',
     startTimestamp: browsingTimestamp,
     type: ActivityType.Watching,
   }
