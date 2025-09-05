e688ea25360ba668874a448466db4acf5424ac38
omstr <omid56@live.co.uk>
2025-09-05 16:59:24 +0100

    feat(combi.fm) - add activity

--- websites/C/combi.fm/presence.ts
+++ websites/C/combi.fm/presence.ts
@@ -12,6 +12,28 @@ enum ActivityAssets { // Other default assets can be found at index.d.ts
   SC_Logo = 'https://d21buns5ku92am.cloudfront.net/26628/images/419679-1x1_SoundCloudLogo_cloudmark-f5912b-original-1645807040.jpg',
 }
 
+interface Settings {
+  title: boolean
+  artists: boolean
+  constant: boolean
+  friends: boolean
+  clear: boolean
+}
+
+interface HandleSettingsParams {
+  settings: Settings
+  bHidden?: boolean
+  trackTitle: string
+  artistsContent?: string | null
+}
+
+interface HandleSettingsResult {
+  name: string | null
+  details: string | null
+  state: string | null
+  bHidden: boolean
+}
+
 function parseToSeconds(time?: string | undefined | null): number | undefined {
   if (!time)
     return undefined
@@ -32,14 +54,65 @@ function parseToSeconds(time?: string | undefined | null): number | undefined {
   return mins * 60 + secs
 }
 
+function handleSettings({
+  settings,
+  bHidden,
+  trackTitle,
+  artistsContent,
+}: HandleSettingsParams) {
+  const { title, artists } = settings
+
+  let name: string | null
+  let details: string | null
+  let state: string | null
+
+  // Title setting
+  if (title || artists) {
+    bHidden = true
+    if (artists) {
+      details = `${trackTitle} | combi.fm`
+      if (artistsContent) {
+        name = artistsContent
+        state = artistsContent
+      }
+      else {
+        name = `${trackTitle} | combi.fm`
+        state = 'Listening on combi.fm'
+      }
+    }
+    else {
+      name = trackTitle
+      details = `${trackTitle} | combi.fm`
+      if (artistsContent) {
+        state = artistsContent
+      }
+      else {
+        state = 'Listening on combi.fm'
+      }
+    }
+  }
+  else {
+    name = 'combi.fm'
+    details = trackTitle
+    state = null
+    if (artistsContent) {
+      state = artistsContent
+    }
+  }
+
+  return { name, details, state, bHidden }
+}
+
 let timerId: ReturnType<typeof setTimeout> | null = null
-let bPausedLongTime = false
+let bPausedLongTime: boolean = false
+let oldTitle: string | null = null
 
 presence.on('UpdateData', async () => {
+  const logo = document.querySelector('#header-logo') as HTMLImageElement ?? ActivityAssets.Logo
   const presenceData: PresenceData = {
     name: 'combi.fm',
-    largeImageKey: ActivityAssets.Logo,
-    smallImageKey: ActivityAssets.Logo,
+    largeImageKey: logo,
+    smallImageKey: logo,
     smallImageText: 'combi.fm',
     type: ActivityType.Listening,
     buttons: [
@@ -51,11 +124,6 @@ presence.on('UpdateData', async () => {
   }
 
   const trackTitle = document.querySelector('#footer-track-title')?.textContent
-
-  if (!trackTitle) {
-    return presence.clearActivity()
-  }
-
   // console.log("Track title: ", trackTitle);
   const trackImgEl = document.querySelector('#footer-track-img') as HTMLImageElement
   const artistsContent = document.querySelector<HTMLElement>('#footer-track-artists')?.textContent
@@ -66,27 +134,54 @@ presence.on('UpdateData', async () => {
   const sp_platform = document.querySelector<HTMLElement>('#footer-platform-spotify')
   const sc_platform = document.querySelector<HTMLElement>('#footer-platform-soundcloud')
 
+  // Settings
+  const [titleSetting, artistsSetting, clearSetting, constantSetting, friendsSetting] = await Promise.all([
+    presence.getSetting<boolean>('title'),
+    presence.getSetting<boolean>('artists'),
+    presence.getSetting<boolean>('clear'),
+    presence.getSetting<boolean>('constant'),
+    presence.getSetting<boolean>('friends'),
+  ])
+
+  if (!trackTitle) {
+    if (constantSetting) {
+      return await presence.setActivity({
+        type: ActivityType.Playing,
+        largeImageKey: logo,
+        details: null,
+        state: 'Paused',
+      })
+    }
+    return presence.clearActivity()
+  }
+
   if (paused) {
     if (timerId)
       return
 
     const result = await new Promise<boolean>((resolve) => {
       timerId = setTimeout(() => {
-        bPausedLongTime = Boolean(paused) // bPausedLongTime is whatever `paused` is 5 second later
+        bPausedLongTime = Boolean(paused) // bPausedLongTime is whatever `paused` is 2 second later
         resolve(bPausedLongTime)
         timerId = null
-      }, 5000)
+      }, 2000)
     })
 
-    // If still paused after 1 second:
+    // If still paused:
     if (result) {
       // If presence doesn't clear properly
-      // await presence.setActivity({
-      //   type: ActivityType.Playing,
-      //   largeImageKey: ActivityAssets.Logo,
-      //   details: null,
-      //   state: "Paused",
-      // });
+      if (constantSetting) {
+        const detailsMsg = oldTitle ? `Last listened to: \n ${oldTitle}` : null
+        if (!(oldTitle || oldTitle === trackTitle) && trackTitle) {
+          oldTitle = trackTitle
+        }
+        return await presence.setActivity({
+          type: ActivityType.Playing,
+          largeImageKey: logo,
+          details: detailsMsg,
+          state: 'Paused',
+        })
+      }
 
       return presence.clearActivity()
     }
@@ -98,53 +193,37 @@ presence.on('UpdateData', async () => {
 
   [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(timestampFromFormat(currentTime ?? ''), timestampFromFormat(totalDuration ?? ''))
 
-  // Settings
-  const [titleSetting, artistsSetting, clearSetting] = await Promise.all([
-    presence.getSetting<boolean>('title'),
-    presence.getSetting<boolean>('artists'),
-    presence.getSetting<boolean>('clear'),
-  ])
   // Clear setting
   if (clearSetting) {
     return presence.clearActivity()
   }
-  // Title setting
-  if (titleSetting || artistsSetting) {
-    if (artistsSetting) {
-      presenceData.details = `${trackTitle} via combi.fm`
-      if (artistsContent) {
-        presenceData.name = artistsContent
-        presenceData.state = artistsContent
-      }
-      else {
-        presenceData.name = `${trackTitle} via combi.fm`
-        presenceData.state = 'Listening on combi.fm'
-      }
-    }
-    else {
-      presenceData.name = trackTitle
-      presenceData.details = `${trackTitle} via combi.fm`
-      if (artistsContent) {
-        presenceData.state = artistsContent
-      }
-      else {
-        presenceData.state = 'Listening on combi.fm'
-      }
-    }
-  }
-  else {
-    presenceData.name = 'combi.fm'
-    presenceData.details = trackTitle
-    if (artistsContent) {
-      presenceData.state = artistsContent
-    }
-  }
 
-  if (sc_platform) {
+  const { name, details, state, bHidden } = handleSettings({
+    settings: {
+      title: titleSetting,
+      artists: artistsSetting,
+      friends: friendsSetting,
+      constant: constantSetting,
+      clear: clearSetting,
+    },
+    trackTitle,
+    artistsContent,
+    bHidden: false,
+  })
+
+  presenceData.name = name
+  presenceData.details = details
+  presenceData.state = state
+
+  if (bHidden) {
+    presenceData.smallImageKey = ActivityAssets.Logo
+    presenceData.smallImageText = 'combi.fm'
+  }
+  else if (sc_platform) {
     presenceData.smallImageKey = ActivityAssets.SC_Logo
     presenceData.smallImageText = 'SoundCloud'
   }
-  if (sp_platform) {
+  else if (sp_platform) {
     presenceData.smallImageKey = ActivityAssets.SP_Logo
     presenceData.smallImageText = 'Spotify'
   }
