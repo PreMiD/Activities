e2da2eae38efbd266ae09d2b2517fb3d61b29cac
Al3ss1Am0d <131905914+Al3ss1Am0d@users.noreply.github.com>
2025-09-19 17:08:45 +0200

    Update presence.ts
    Signed-off-by: Al3ss1Am0d <131905914+Al3ss1Am0d@users.noreply.github.com>

--- websites/E/Easyfun/presence.ts
+++ websites/E/Easyfun/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.postimg.cc/nzGR3SrC/263cef731b755103d3a010a8941458d7-1.png',
+  Logo = 'https://th.bing.com/th/id/ODF.2fU0nE7YI6b9b_lj-9F9QA?w=152&h=152&qlt=100&pcl=fffffa&o=6&pid=1.5.png',
 }
 
 presence.on('UpdateData', async () => {
@@ -21,15 +21,15 @@ presence.on('UpdateData', async () => {
       .map(word =>
         /^\d+$/.test(word)
           ? word
-          : word.charAt(0).toUpperCase() + word.slice(1),
+          : word.charAt(0).toUpperCase() + word.slice(1)
       )
       .join(' ')
   }
 
-  const gameIcon = document.querySelector('img[alt$="-icon"]')?.getAttribute('src')
+  const gameIcon = document.querySelector('img[data-nimg="1"]')?.getAttribute('src') ?? ActivityAssets.Logo
 
   const presenceData: PresenceData = {
-    largeImageKey: gameIcon ?? undefined,
+    largeImageKey: gameIcon,
     smallImageKey: ActivityAssets.Logo,
     details: gameName ? `Playing ${gameName}` : 'Exploring EasyFun',
     state: gameName ? 'Cloud Gaming' : 'Browsing on site',
