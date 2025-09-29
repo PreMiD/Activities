601c4c292da9f12dc279e5b5bc39060e732c5667
Chackakaca <chackakaca@gmail.com>
2025-09-28 19:14:41 -0700

    Ultrabox Activity
    I saw that there was only an activity for Jummbox, so I made one for Ultrabox. It shows the tab's Title to display the song name and shows how long you've had the tab open.

---
+++ websites/U/Ultrabox/presence.ts
@@ -0,0 +1,33 @@
+const presence = new Presence({
+  clientId: '1422033396233932900'
+})
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/zKibaZa.png'
+}
+
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo
+  }
+
+  // Get the current URL path
+  const path = document.location.pathname
+  const title = document.title;
+  presenceData.details = title
+  presenceData.state = "Composing a song"
+  
+
+  // Add a timestamp to show how long the user has been on the page
+  presenceData.startTimestamp = browsingTimestamp
+
+  // Set the activity
+  if (presenceData.details) {
+    presence.setActivity(presenceData)
+  }
+  else {
+    presence.clearActivity()
+  }
+})
