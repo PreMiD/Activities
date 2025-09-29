12cd4ed2869f78efda0bf5ffa80582913f0ef485
Chackakaca <chackakaca@gmail.com>
2025-09-28 20:03:25 -0700

    Update presence.ts

--- websites/U/Ultrabox/presence.ts
+++ websites/U/Ultrabox/presence.ts
@@ -1,25 +1,21 @@
 const presence = new Presence({
-  clientId: '1422033396233932900'
+  clientId: '1422033396233932900',
 })
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/zKibaZa.png'
+  Logo = 'https://i.imgur.com/zKibaZa.png',
 }
 
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    largeImageKey: ActivityAssets.Logo
+    largeImageKey: ActivityAssets.Logo,
   }
 
-  // Get the current URL path
-  const path = document.location.pathname
-  const title = document.title;
+  const title = document.title
   presenceData.details = title
-  presenceData.state = "Composing a song"
-  
-
+  presenceData.state = 'Composing a song'
   // Add a timestamp to show how long the user has been on the page
   presenceData.startTimestamp = browsingTimestamp
 
