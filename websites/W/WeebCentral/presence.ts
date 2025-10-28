5999b3e9951839ee4d06a86a85fcb1ae795d6e57
Pchaozz <polsh2001.1@gmail.com>
2025-10-28 22:51:23 +0100

    WeebCentral activity working
    - Shows manga, chapters and browsing without errors

---
+++ websites/W/WeebCentral/presence.ts
@@ -0,0 +1,82 @@
+import { Assets } from 'premid'
+
+const presence = new Presence({
+  clientId: '1432815872891683019',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets { // Other default assets can be found at index.d.ts
+  Logo = '',
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+    smallImageKey: Assets.Play,
+  }
+
+  const { pathname } = window.location
+
+  //CHOSING CHAPTER ON A SPECIFIC MANGA
+  if (pathname.startsWith('/series/')) {
+    const mangaTitle = document.querySelector('h1.text-2xl')?.textContent?.trim() || document.querySelector('meta[property="og:title"]')?.getAttribute("content");
+    const mangaCover = document.querySelector("picture img")?.getAttribute("src") || document.querySelector('meta[property="og:image"]')?.getAttribute("srcset");
+
+    presenceData.details = `Ready to read: ${mangaTitle}`;
+    presenceData.state = 'Chossing chapter';
+
+    if (mangaCover) presenceData.largeImageKey = mangaCover;
+
+    presenceData.buttons = [
+      {
+        label: "View on WeebCentral",
+        url: window.location.href
+      },
+    ];
+  }
+  
+  //SPECIFIC CHAPTER
+  else if (pathname.startsWith('/chapters/')) {
+    const ogTitle = document.querySelector('meta[property="og:title"]')?.getAttribute("content");
+    const ogImage = document.querySelector('meta[property="og:image"]')?.getAttribute("content");
+    const preloadImage = document.querySelector('link[rel="preload"][as="image"]')?.getAttribute('href');
+
+    let chapterTitle = "Error reading title";
+    let mangaTitle = "Error reading manga";
+
+    if (ogTitle) {
+      const parts = ogTitle.split("|").map((x) => x.trim());
+
+      chapterTitle = parts[0] || chapterTitle;
+      mangaTitle = parts[1] || mangaTitle;
+    }
+
+    const chapterImage = preloadImage || ogImage || Assets.Reading;
+
+    presenceData.details = `Reading: ${mangaTitle}`;
+    presenceData.state = chapterTitle;
+    presenceData.largeImageKey = chapterImage;
+    presenceData.smallImageKey = Assets.Reading;
+
+    presenceData.buttons = [
+      {
+        label: "View on WeebCentral",
+        url: window.location.href
+      },
+    ];
+  }
+
+  // MAIN PAGE
+  else if (pathname === '/' || pathname.startsWith('/search')) {
+    presenceData.details = 'Searching something to read...' 
+    presenceData.state = 'On WeebCentral'
+  } 
+
+  else {
+    presenceData.details = 'Browsing WeebCentral' 
+    presenceData.state = 'Exploring manga world'
+  }
+
+  presence.setActivity(presenceData)
+})
