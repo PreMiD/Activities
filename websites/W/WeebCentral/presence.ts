e38cf8bf630409597262a3ec603e01b4b64c2774
Pchaozz <polsh2001.1@gmail.com>
2025-10-29 13:51:21 +0100

    Cleaned the code again
    - Fixing things to get ESLint test without errors

--- websites/W/WeebCentral/presence.ts
+++ websites/W/WeebCentral/presence.ts
@@ -20,18 +20,20 @@ presence.on('UpdateData', async () => {
   const { pathname } = window.location
 
   if (pathname.startsWith('/series/')) {
-    const mangaTitle =
-      document.querySelector('h1.text-2xl')?.textContent?.trim() ||
-      document.querySelector('meta[property="og:title"]')?.getAttribute('content')
+    const mangaTitle 
+      = document.querySelector('h1.text-2xl')?.textContent?.trim() 
+      || document.querySelector('meta[property="og:title"]')?.getAttribute('content')
 
-    const mangaCover =
-      document.querySelector('picture img')?.getAttribute('src') ||
-      document.querySelector('meta[property="og:image"]')?.getAttribute('content')
+    const mangaCover 
+      = document.querySelector('picture img')?.getAttribute('src') 
+      || document.querySelector('meta[property="og:image"]')?.getAttribute('content')
 
     presenceData.details = `Ready to read: ${mangaTitle}`
     presenceData.state = 'Choosing chapter'
 
-    if (mangaCover) presenceData.largeImageKey = mangaCover
+    if (mangaCover) {
+      presenceData.largeImageKey = mangaCover
+    }
 
     presenceData.buttons = [
       {
@@ -39,7 +41,8 @@ presence.on('UpdateData', async () => {
         url: window.location.href,
       },
     ]
-  } else if (pathname.startsWith('/chapters/')) {
+  } 
+  else if (pathname.startsWith('/chapters/')) {
     const ogTitle = document.querySelector('meta[property="og:title"]')?.getAttribute('content')
     const ogImage = document.querySelector('meta[property="og:image"]')?.getAttribute('content')
     const preloadImage = document
@@ -68,10 +71,12 @@ presence.on('UpdateData', async () => {
         url: window.location.href,
       },
     ]
-  } else if (pathname === '/' || pathname.startsWith('/search')) {
+  } 
+  else if (pathname === '/' || pathname.startsWith('/search')) {
     presenceData.details = 'Searching something to read...'
     presenceData.state = 'On WeebCentral'
-  } else {
+  } 
+  else {
     presenceData.details = 'Browsing WeebCentral'
     presenceData.state = 'Exploring new manga'
   }
