0bb833715353932ec938f2783a24f78137ed08fe
Pchaozz <polsh2001.1@gmail.com>
2025-10-29 13:35:09 +0100

    Cleaned code
    - Adjusted the code to pass ESLint test

--- websites/W/WeebCentral/presence.ts
+++ websites/W/WeebCentral/presence.ts
@@ -3,10 +3,11 @@ import { Assets } from 'premid'
 const presence = new Presence({
   clientId: '1432815872891683019',
 })
+
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
-enum ActivityAssets { // Other default assets can be found at index.d.ts
-  Logo = '',
+const ActivityAssets = {
+  Logo: '',
 }
 
 presence.on('UpdateData', async () => {
@@ -18,64 +19,61 @@ presence.on('UpdateData', async () => {
 
   const { pathname } = window.location
 
-  //CHOSING CHAPTER ON A SPECIFIC MANGA
   if (pathname.startsWith('/series/')) {
-    const mangaTitle = document.querySelector('h1.text-2xl')?.textContent?.trim() || document.querySelector('meta[property="og:title"]')?.getAttribute("content");
-    const mangaCover = document.querySelector("picture img")?.getAttribute("src") || document.querySelector('meta[property="og:image"]')?.getAttribute("srcset");
+    const mangaTitle =
+      document.querySelector('h1.text-2xl')?.textContent?.trim() ||
+      document.querySelector('meta[property="og:title"]')?.getAttribute('content')
 
-    presenceData.details = `Ready to read: ${mangaTitle}`;
-    presenceData.state = 'Chossing chapter';
+    const mangaCover =
+      document.querySelector('picture img')?.getAttribute('src') ||
+      document.querySelector('meta[property="og:image"]')?.getAttribute('content')
 
-    if (mangaCover) presenceData.largeImageKey = mangaCover;
+    presenceData.details = `Ready to read: ${mangaTitle}`
+    presenceData.state = 'Choosing chapter'
+
+    if (mangaCover) presenceData.largeImageKey = mangaCover
 
     presenceData.buttons = [
       {
-        label: "View on WeebCentral",
-        url: window.location.href
+        label: 'View on WeebCentral',
+        url: window.location.href,
       },
-    ];
-  }
-  
-  //SPECIFIC CHAPTER
-  else if (pathname.startsWith('/chapters/')) {
-    const ogTitle = document.querySelector('meta[property="og:title"]')?.getAttribute("content");
-    const ogImage = document.querySelector('meta[property="og:image"]')?.getAttribute("content");
-    const preloadImage = document.querySelector('link[rel="preload"][as="image"]')?.getAttribute('href');
+    ]
+  } else if (pathname.startsWith('/chapters/')) {
+    const ogTitle = document.querySelector('meta[property="og:title"]')?.getAttribute('content')
+    const ogImage = document.querySelector('meta[property="og:image"]')?.getAttribute('content')
+    const preloadImage = document
+      .querySelector('link[rel="preload"][as="image"]')
+      ?.getAttribute('href')
 
-    let chapterTitle = "Error reading title";
-    let mangaTitle = "Error reading manga";
+    let chapterTitle = 'Unknown chapter title'
+    let mangaTitle = 'Unknown manga'
 
     if (ogTitle) {
-      const parts = ogTitle.split("|").map((x) => x.trim());
-
-      chapterTitle = parts[0] || chapterTitle;
-      mangaTitle = parts[1] || mangaTitle;
+      const parts = ogTitle.split('|').map(x => x.trim())
+      chapterTitle = parts[0] || chapterTitle
+      mangaTitle = parts[1] || mangaTitle
     }
 
-    const chapterImage = preloadImage || ogImage || Assets.Reading;
+    const chapterImage = ogImage || preloadImage || Assets.Reading
 
-    presenceData.details = `Reading: ${mangaTitle}`;
-    presenceData.state = chapterTitle;
-    presenceData.largeImageKey = chapterImage;
-    presenceData.smallImageKey = Assets.Reading;
+    presenceData.details = `Reading: ${mangaTitle}`
+    presenceData.state = chapterTitle
+    presenceData.largeImageKey = chapterImage
+    presenceData.smallImageKey = Assets.Reading
 
     presenceData.buttons = [
       {
-        label: "View on WeebCentral",
-        url: window.location.href
+        label: 'View on WeebCentral',
+        url: window.location.href,
       },
-    ];
-  }
-
-  // MAIN PAGE
-  else if (pathname === '/' || pathname.startsWith('/search')) {
-    presenceData.details = 'Searching something to read...' 
+    ]
+  } else if (pathname === '/' || pathname.startsWith('/search')) {
+    presenceData.details = 'Searching something to read...'
     presenceData.state = 'On WeebCentral'
-  } 
-
-  else {
-    presenceData.details = 'Browsing WeebCentral' 
-    presenceData.state = 'Exploring manga world'
+  } else {
+    presenceData.details = 'Browsing WeebCentral'
+    presenceData.state = 'Exploring new manga'
   }
 
   presence.setActivity(presenceData)
