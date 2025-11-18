1192a24322aa8181c5609cf5c6d7f9f1d948213a
Marcos Ferreira <71356853+rfxct@users.noreply.github.com>
2025-11-17 22:54:39 -0300

    feat:([untitled]): add support for untitled.stream

---
+++ websites/#/[untitled]/presence.ts
@@ -0,0 +1,58 @@
+import { ActivityType, getTimestamps, timestampFromFormat } from 'premid'
+
+const presence = new Presence({
+  clientId: '1440157128500314122',
+})
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/mQBrqv7.png',
+}
+
+function getElementText(selector: string) {
+  const element = document.querySelector(selector)
+  return element?.textContent?.trim() ?? ''
+}
+
+function isPlaying() {
+  const audioTag = document.querySelector<HTMLAudioElement>('body > audio')
+
+  return !!audioTag && audioTag?.paused === false
+}
+
+presence.on('UpdateData', async () => {
+  if (!isPlaying()) {
+    return presence.clearActivity()
+  }
+
+  const showLogo = await presence.getSetting<boolean>('show-logo')
+
+  const title = getElementText('.playbar-title')
+  const subtitle = getElementText('.playbar-subtitle')
+  const coverArt = document.querySelector<HTMLImageElement>('#playbar img')?.src
+
+  const presenceData: PresenceData = {
+    name: title,
+    details: title,
+    state: subtitle,
+    type: ActivityType.Listening,
+    largeImageKey: coverArt,
+  }
+
+  if (showLogo) {
+    Object.assign(presenceData, {
+      smallImageKey: ActivityAssets.Logo,
+      smallImageUrl: 'https://untitled.stream',
+      smallImageText: '[untitled]',
+      largeImageText: 'Listening on [untitled]'
+    })
+  }
+
+  const timestamp = getElementText('.timestamp').split(' / ')
+  if (timestamp.length === 2) {
+    const [currentTime, duration] = timestamp.map(timestampFromFormat);
+
+    [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(currentTime!, duration!)
+  }
+
+  presence.setActivity(presenceData)
+})
