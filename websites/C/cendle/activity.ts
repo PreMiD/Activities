0b334405d1b33f24c53bef89b1a1d7445bef7ff7
Kacper <loganhyhy@gmail.com>
2025-11-18 18:23:32 +0100

    Refactor string literals to use template literals
    Signed-off-by: Kacper <loganhyhy@gmail.com>

--- websites/C/cendle/activity.ts
+++ websites/C/cendle/activity.ts
@@ -1,42 +1,42 @@
-import type { PresenceData } from '@types/premid';
-import { Activity } from '@types/premid';
+import type { PresenceData } from '@types/premid'
+import { Activity } from '@types/premid'
 
 export default class CendleActivity extends Activity {
   override async onUrl(url: URL): Promise<PresenceData | null> {
-    const mode = url.searchParams.get('mode');
+    const mode = url.searchParams.get('mode')
 
-    let details = 'Cendle.pl';
-    let state = 'Codzienna gra o zgadywaniu cen';
+    let details = `Cendle.pl`
+    let state = `Codzienna gra o zgadywaniu cen`
 
     switch (mode) {
-      case 'guess':
-        details = 'Zgadnij cenę';
-        state = 'Próbuje zgadnąć produkt';
-        break;
+      case `guess`:
+        details = `Zgadnij cenę`
+        state = `Próbuje zgadnąć produkt`
+        break
 
-      case 'promo':
-        details = 'Promocja czy nie';
-        state = 'Sprawdza promocję';
-        break;
+      case `promo`:
+        details = `Promocja czy nie`
+        state = `Sprawdza promocję`
+        break
 
-      case 'compare':
-        details = 'Który produkt droższy?';
-        state = 'Porównuje ceny';
-        break;
+      case `compare`:
+        details = `Który produkt droższy?`
+        state = `Porównuje ceny`
+        break
 
-      case 'sum':
-        details = 'Ile razem?';
-        state = 'Liczy wartość koszyka';
-        break;
+      case `sum`:
+        details = `Ile razem?`
+        state = `Liczy wartość koszyka`
+        break
 
       default:
-        break;
+        break
     }
 
     return {
       details,
       state,
-      largeImageKey: 'icon'
-    };
+      largeImageKey: `icon`
+    }
   }
 }
