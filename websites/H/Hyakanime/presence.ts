fe1ca61df64ce6baa2293091273b6337ff21bf26
Kaan <kaantopkaya32@gmail.com>
2025-11-29 20:43:57 +0100

    fix(hyakanime): fix logo size and version

--- websites/H/Hyakanime/presence.ts
+++ websites/H/Hyakanime/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/ttdd3Q6.jpeg',
+  Logo = 'https://i.imgur.com/kSomZoz.png',
 }
 
 const routes = {
