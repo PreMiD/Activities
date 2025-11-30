bbd988064f39d54e98ef43aaf60d8e08e82918ad
haruki-pixel <harukisan211@gmail.com>
2025-11-30 15:32:32 +0800

    Update presence.ts
    Signed-off-by: haruki-pixel <harukisan211@gmail.com>

--- websites/B/BEECH/presence.ts
+++ websites/B/BEECH/presence.ts
@@ -1,18 +1,22 @@
-import Presence from "../../presence";
+import Presence from '../../presence';
 
 const presence = new Presence({
-  clientId: "1444556736647397476" // Palihug ilisdi sa imong Discord App client ID
+  clientId: '1444556736647397476'
 });
 
-presence.on("UpdateData", async () => {
-  // Example: get video title and paused state
-  const videoTitle = document.querySelector(".video-title")?.textContent;
-  const paused = document.querySelector(".play-button.paused") !== null;
+presence.on('UpdateData', async () => {
+  const videoTitle =
+    document.querySelector('.video-title')?.textContent || null;
+
+  const paused =
+    document.querySelector('.play-button.paused') !== null;
 
   presence.setActivity({
-    details: videoTitle || "Explore a vast collection of movies and TV shows across all genres.",
-    state: paused ? "Paused" : "BEECH.WATCH",
-    largeImageKey: "beech",
+    details:
+      videoTitle ||
+      'Explore a vast collection of movies and TV shows across all genres.',
+    state: paused ? 'Paused' : 'BEECH.WATCH',
+    largeImageKey: 'beech',
     instance: false
   });
 });
