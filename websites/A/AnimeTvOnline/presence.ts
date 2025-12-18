4c82e3ab1f1312df2ba311fe10e2d8356b247748
D3rkPow4 <89229260+D3rkPow4@users.noreply.github.com>
2025-12-18 15:09:36 +0100

    Update presence.ts
    Signed-off-by: D3rkPow4 <89229260+D3rkPow4@users.noreply.github.com>

--- websites/A/AnimeTvOnline/presence.ts
+++ websites/A/AnimeTvOnline/presence.ts
@@ -1,42 +1,44 @@
-import { Presence } from "premid";
+import { Presence } from 'premid'
 
 const presence = new Presence({
-    clientId: "1017558325753303102"
-});
+  clientId: '1017558325753303102'
+})
 
-presence.on("UpdateData", async () => {
-    const dataDiv = document.getElementById("premid-data");
+presence.on('UpdateData', async () => {
+  const dataDiv = document.getElementById('premid-data')
 
-    if (dataDiv) {
-        return {
-            largeImageKey: "logo_grande",
-            largeImageText: dataDiv.dataset.anime,
-            details: dataDiv.dataset.anime,
-            state: `Guardando l'episodio ${dataDiv.dataset.episode}`,
-            buttons: [
-                {
-                    label: "Guarda anche tu",
-                    url: document.location.href
-                },
-                {
-                    label: "Scheda Anime",
-                    url: `https://animetvonline.org/dettagli.php?slug=${dataDiv.dataset.slug}`
-                }
-            ]
-        };
+  // 1. Se siamo nel Player
+  if (dataDiv) {
+    return {
+      largeImageKey: 'logo_grande',
+      largeImageText: dataDiv.dataset.anime,
+      details: dataDiv.dataset.anime,
+      state: `Guardando l'episodio ${dataDiv.dataset.episode}`,
+      buttons: [
+        {
+          label: 'Guarda anche tu',
+          url: document.location.href
+        },
+        {
+          label: 'Scheda Anime',
+          url: `https://animetvonline.org/dettagli.php?slug=${dataDiv.dataset.slug}`
+        }
+      ]
     }
+  }
 
-    if (document.location.pathname === "/" || document.location.pathname === "/index.php") {
-        return {
-            largeImageKey: "logo_grande",
-            details: "Navigando nella Home",
-            state: "Cercando un anime...",
-            buttons: [
-                {
-                    label: "Visita il sito",
-                    url: "https://animetvonline.org"
-                }
-            ]
-        };
+  // 2. Se siamo nella Home
+  if (document.location.pathname === '/' || document.location.pathname === '/index.php') {
+    return {
+      largeImageKey: 'logo_grande',
+      details: 'Navigando nella Home',
+      state: 'Cercando un anime...',
+      buttons: [
+        {
+          label: 'Visita il sito',
+          url: 'https://animetvonline.org'
+        }
+      ]
     }
-});
+  }
+})
