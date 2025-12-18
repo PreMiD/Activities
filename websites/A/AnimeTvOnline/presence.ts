db774b6017cde02855ebef5338cbf5afda1f53be
D3rkPow4 <89229260+D3rkPow4@users.noreply.github.com>
2025-12-19 00:34:56 +0100

    Update presence.ts
    Signed-off-by: D3rkPow4 <89229260+D3rkPow4@users.noreply.github.com>

--- websites/A/AnimeTvOnline/presence.ts
+++ websites/A/AnimeTvOnline/presence.ts
@@ -1,6 +1,6 @@
 import * as PreMiD from 'premid'
 
-// eslint-disable-next-line
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const presence = new (PreMiD as any).Presence({
   clientId: '1017558325753303102',
 })
