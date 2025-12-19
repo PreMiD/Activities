26f7ce8dd8d965433003215fe781f21f95c18c09
joerkig <joeri.rozema@gmail.com>
2025-12-19 19:34:58 +0100

    feat(Global Player): add activity

--- websites/G/Global Player/presence.ts
+++ websites/G/Global Player/presence.ts
@@ -1,4 +1,4 @@
-import { ActivityType, Assets, StatusDisplayType } from 'premid'
+import { ActivityType, Assets, getTimestampsFromMedia, StatusDisplayType } from 'premid'
 
 const presence = new Presence({
   clientId: '1449716575367204974',
@@ -16,35 +16,127 @@ presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
     largeImageKey: ActivityAssets.Logo,
     startTimestamp: browsingTimestamp,
-    smallImageKey: Assets.Live,
     type: ActivityType.Listening,
   }
 
-  const imageElement = document.querySelector<HTMLImageElement>('[class^="style_playbarInfo"] [class^="style_image"]')
+  const playbar = document.querySelector('[class^="style_playbar"]')
+
+  let image
+  const imageElement = playbar?.querySelector<HTMLImageElement>('[class^="style_image"]')
   if (imageElement?.src) {
-    presenceData.largeImageKey = await getImageDataUrl(imageElement)
+    image = await getImageDataUrl(imageElement)
   }
 
-  presenceData.details = document.querySelector<HTMLDivElement>('[class^="style_playbarInfo"] [class^="style_title"] b')?.textContent ?? document.querySelector<HTMLDivElement>('[class^="style_playbarInfo"] [class^="style_title"]')?.textContent
-  presenceData.state = document.querySelector<HTMLDivElement>('[class^="style_playbarInfo"] [class^="playbar-title_trackArtist"]')?.textContent
-  presenceData.name = document.querySelector<HTMLDivElement>('[class^="style_playbarInfo"] [class^="style_subtitleLink"]')?.textContent
+  const title = playbar?.querySelector('[class^="style_title"] b')?.textContent ?? document.querySelector('[class^="style_title"]')?.textContent
+  const artist = playbar?.querySelector('[class^="playbar-title_trackArtist"]')?.textContent
+  const subTitle = playbar?.querySelector('[class^="style_subtitleLink"]')?.textContent ?? document.querySelector('[class^="style_subtitle"]')?.textContent
+  const subtitleLink = playbar?.querySelector<HTMLAnchorElement>('[class^="style_subtitleLink"]')
+  const playbarBadge = playbar?.querySelector('[class^="style_playbarBadge"]')?.textContent?.toLowerCase().replaceAll(' ', '')
 
-  presenceData.statusDisplayType = StatusDisplayType.State
-
-  switch (document.querySelector<HTMLDivElement>('[class^="style_playbarInfo"] [class^="style_playbarBadge"]')?.textContent?.toLowerCase().replaceAll(' ', '')) {
-    case 'live':{
-      const subtitleLink = document.querySelector<HTMLAnchorElement>('[class^="style_playbarInfo"] [class^="style_subtitleLink"]')
-      if (subtitleLink) {
-        presenceData.buttons = [{
-          label: 'Listen to Radio',
-          url: subtitleLink.href,
-        }]
+  if (playbar?.querySelector<HTMLButtonElement>('[class*="style_actionButton"]')?.title !== 'Play') {
+    switch (playbarBadge) {
+      case 'live':{
+        presenceData.details = subTitle
+        presenceData.state = title
+        presenceData.largeImageText = artist
+        presenceData.largeImageKey = image
+        presenceData.statusDisplayType = StatusDisplayType.Details
+        if (subtitleLink) {
+          presenceData.buttons = [{
+            label: 'View Radio',
+            url: subtitleLink.href,
+          }]
+        }
+        presenceData.smallImageKey = Assets.Live
+        presenceData.smallImageText = 'Live'
+        break
       }
-      break
+      case 'liveplaylist':{
+        presenceData.details = subTitle
+        presenceData.state = title
+        presenceData.largeImageText = artist
+        presenceData.largeImageKey = image
+        presenceData.statusDisplayType = StatusDisplayType.Details
+        if (subtitleLink) {
+          presenceData.buttons = [{
+            label: 'View Playlist',
+            url: subtitleLink.href,
+          }]
+        }
+        presenceData.detailsUrl = subtitleLink?.href
+        break
+      }
+      case 'podcast':{
+        presenceData.details = subTitle
+        presenceData.state = title
+        presenceData.largeImageKey = image
+        presenceData.statusDisplayType = StatusDisplayType.Details
+        presenceData.smallImageKey = Assets.Play
+        const video = document.querySelector<HTMLVideoElement>('video')
+        if (video) {
+          [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestampsFromMedia(video)
+        }
+        if (subtitleLink) {
+          presenceData.buttons = [{
+            label: 'View Podcast',
+            url: subtitleLink.href,
+          }]
+        }
+        presenceData.detailsUrl = subtitleLink?.href
+        break
+      }
+      case 'catchup':{
+        presenceData.details = title
+        presenceData.state = subTitle
+        presenceData.largeImageKey = image
+        presenceData.statusDisplayType = StatusDisplayType.Details
+        const video = document.querySelector<HTMLVideoElement>('video')
+        if (video) {
+          if (!video.paused) {
+            [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestampsFromMedia(video)
+          }
+          presenceData.smallImageKey = Assets.Play
+        }
+        break
+      }
+      case 'replay':{
+        presenceData.details = subTitle
+        presenceData.state = title
+        presenceData.largeImageText = artist
+        presenceData.largeImageKey = image
+        presenceData.statusDisplayType = StatusDisplayType.Details
+        if (subtitleLink) {
+          presenceData.buttons = [{
+            label: 'View Radio',
+            url: subtitleLink.href,
+          }]
+        }
+        presenceData.smallImageKey = Assets.Repeat
+        presenceData.smallImageText = 'Replay'
+        break }
+      default:{
+        presenceData.details = playbarBadge
+      }
     }
-    case 'liveplaylist':{
-      break
+  }
+  else if (document.location.pathname.startsWith('/videos')) {
+    const videoDetails = document.querySelector('[class^="style_videoDetails"]')
+    presenceData.details = videoDetails?.querySelector('[class^="style_title"]')?.textContent
+    presenceData.state = videoDetails?.querySelector('[class^="style_timeAgo"]')?.textContent
+    presenceData.buttons = [{
+      label: 'Watch Video',
+      url: document.location.href,
+    }]
+    const video = document.querySelector<HTMLVideoElement>('#globalVideoPlayer video')
+    if (video) {
+      if (!video.paused)
+        [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestampsFromMedia(video)
+      presenceData.smallImageKey = video?.paused ? Assets.Pause : Assets.Play
     }
+  }
+  else {
+    presenceData.details = 'Browsing'
+    presenceData.state = document.querySelector('.menu-items [class*="style_selected"]')?.textContent
   }
 
   async function getImageDataUrl(imageElement: HTMLImageElement): Promise<string> {
