8edeccca98b44d9ac8ff8f4a2301d00157da34c3
Diego <daspking@gmail.com>
2025-12-22 21:51:07 -0300

    fix(AnimeAV1): fix linting errors

--- websites/A/AnimeAv1/iframe.ts
+++ websites/A/AnimeAv1/iframe.ts
@@ -1,51 +1,26 @@
-const iframe = new iFrame();
+const iframe = new iFrame()
 
-let lastSent: any = null;
+iframe.on('UpdateData', async () => {
+  const video = document.querySelector('video')
 
-function sanitizeImage(url: string | null) {
-	if (!url) return null;
-	try {
-		url = String(url);
-		if (/^data:/i.test(url)) return null;
-		if (/^https?:\/\//i.test(url)) return url;
-	} catch {
-		return null;
-	}
-	return null;
-}
+  if (!video) {
+    iframe.send({})
+    return
+  }
 
-function shouldSendUpdate(info: any): boolean {
-	if (!lastSent) return true;
-	if (info.paused !== lastSent.paused) return true;
-	if (Math.abs((info.currentTime || 0) - (lastSent.currentTime || 0)) >= 1)
-		return true;
-	if ((info.duration || 0) !== (lastSent.duration || 0)) return true;
+  try {
+    const data: any = {
+      duration: video.duration,
+      currentTime: video.currentTime,
+      paused: video.paused,
+    }
 
-	return false;
-}
-
-iframe.on("UpdateData", async () => {
-	const video = document.querySelector("video");
-
-	if (!video) {
-		iframe.send({});
-		return;
-	}
-
-	try {
-		const info: any = {
-			duration: video.duration,
-			currentTime: video.currentTime,
-			paused: video.paused,
-		};
-
-		if (!isNaN(info.duration) && !isNaN(info.currentTime)) {
-			if (shouldSendUpdate(info)) {
-				iframe.send(info);
-				lastSent = info;
-			}
-		} else iframe.send({});
-	} catch {
-		iframe.send({});
-	}
-});
+    if (!Number.isNaN(data.duration) && !Number.isNaN(data.currentTime))
+      iframe.send(data)
+    else
+      iframe.send({})
+  }
+  catch {
+    iframe.send({})
+  }
+})
