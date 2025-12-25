122663c64a4c5f9c86926ffd99dd9ca521fbb5a5
Tatsuya <55715737+tatsuya087@users.noreply.github.com>
2025-12-26 07:20:36 +0900

    Update iframe.ts

--- websites/#/アイドルマスターシャイニーカラーズ/iframe.ts
+++ websites/#/アイドルマスターシャイニーカラーズ/iframe.ts
@@ -2,14 +2,14 @@ const iframe = new iFrame()
 let lastUrl = ''
 
 iframe.on('UpdateData', async () => {
-    const { pathname, hash } = document.location
-    const currentUrl = pathname + hash
+  const { pathname, hash } = document.location
+  const currentUrl = pathname + hash
 
-    if (currentUrl !== lastUrl) {
-        lastUrl = currentUrl
-        await iframe.send({
-            pathname,
-            hash,
-        })
-    }
+  if (currentUrl !== lastUrl) {
+    lastUrl = currentUrl
+    await iframe.send({
+      pathname,
+      hash,
+    })
+  }
 })
