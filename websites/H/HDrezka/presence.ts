7b4055593048c1e76003c4d205222fe80ddf5ccd
Artem Shevchenko <81616507+Dyrdoom@users.noreply.github.com>
2026-01-04 18:47:57 +0200

    Update presence.ts
    Signed-off-by: Artem Shevchenko <81616507+Dyrdoom@users.noreply.github.com>

--- websites/H/HDrezka/presence.ts
+++ websites/H/HDrezka/presence.ts
@@ -10,13 +10,13 @@ presence.on('UpdateData', async () => {
     showBrowsingStatus,
     showCover,
     showTimestamp,
-    showSmallImages,
+    showSmallImages
   ] = await Promise.all([
     presence.getSetting<boolean>('privacy'),
     presence.getSetting<boolean>('showBrowsingStatus'),
     presence.getSetting<boolean>('showCover'),
     presence.getSetting<boolean>('timestamp'),
-    presence.getSetting<boolean>('showSmallImages'),
+    presence.getSetting<boolean>('showSmallImages')
   ])
 
   const presenceData: any = {
@@ -30,24 +30,24 @@ presence.on('UpdateData', async () => {
   if (privacyMode && isMediaPage) {
     return presence.setActivity({
       details: 'Watching something private',
-      largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/H/HDrezka/assets/logo.png',
+      largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/H/HDrezka/assets/logo.png'
     })
   }
 
   if (document.location.pathname === '/') {
-    if (!showBrowsingStatus) {
-      return presence.clearActivity()
-    }
+    if (!showBrowsingStatus) return presence.clearActivity()
     presenceData.details = 'Browsing the Home Page'
-  } else if (isMediaPage) {
+  }
+  else if (isMediaPage) {
     const title = document.querySelector('.b-post__title h1')?.textContent?.trim()
     const coverImage = document.querySelector<HTMLImageElement>('.b-sidecover a img')?.src
-
+    
     presenceData.details = title
+    
     presenceData.largeImageKey = (showCover && coverImage) ? coverImage : 'https://cdn.rcd.gg/PreMiD/websites/H/HDrezka/assets/logo.png'
-
+    
     const video = document.querySelector('video')
-
+    
     if (video && !video.paused) {
       if (showTimestamp) {
         const [startTimestamp, endTimestamp] = getTimestampsFromMedia(video)
@@ -55,23 +55,26 @@ presence.on('UpdateData', async () => {
         presenceData.endTimestamp = endTimestamp
       }
       presenceData.type = ActivityType.Watching
-    } else if (showSmallImages) {
-      presenceData.smallImageKey = 'https://cdn.rcd.gg/PreMiD/websites/H/HDrezka/assets/logo.png'
+    }
+    else if (showSmallImages) {
+      presenceData.smallImageKey = 'https://cdn.rcd.gg/PreMiD/websites/H/HDrezka/assets/logo.png' 
       presenceData.smallImageText = 'Paused'
     }
 
     const isSeries = ['series', 'animation', 'cartoons'].includes(contentType || '')
-
+    
     if (isSeries) {
       const activeSeason = document.querySelector('.b-simple_season__item.active')?.textContent?.trim()
       const activeEpisode = document.querySelector('.b-simple_episode__item.active')?.textContent?.trim()
-
+      
       if (activeSeason && activeEpisode) {
         presenceData.state = `Season ${activeSeason}, Episode ${activeEpisode}`
-      } else {
+      }
+      else {
         presenceData.state = 'Watching a series'
       }
-    } else {
+    }
+    else {
       presenceData.state = 'Watching a movie'
     }
 
@@ -80,16 +83,10 @@ presence.on('UpdateData', async () => {
       presenceData.largeImageText = `Voiceover: ${translator}`
     }
 
-    presenceData.buttons = [
-      {
-        label: 'Watch on Rezka',
-        url: document.location.href,
-      },
-    ]
-  } else {
-    if (!showBrowsingStatus) {
-      return presence.clearActivity()
-    }
+    presenceData.buttons = [{ label: 'Watch on Rezka', url: document.location.href }]
+  }
+  else {
+    if (!showBrowsingStatus) return presence.clearActivity()
     presenceData.details = 'Exploring the website'
   }
 
