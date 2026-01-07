5c775e89f419125d62353a1beee2c7555452ac4f
Zinibow <ghostyguy67@gmail.com>
2026-01-06 19:20:30 -0800

    Fixed Code

--- websites/P/PsychonautWiki/presence.ts
+++ websites/P/PsychonautWiki/presence.ts
@@ -1,57 +1,31 @@
-import { Assets } from 'premid'
+const presence = new Presence({
+  clientId: "1432152324163502130",
+});
 
-const browsingTimestamp = Math.floor(Date.now() / 1000)
-
-function updatePresence() {
-  const location = document.location
-  const pathname = location?.pathname ?? ''
-  const search = location?.search ?? ''
-
-  let pageName = 'Homepage'
-  let detailsText = 'Browsing Psychonaut Wiki'
-
-  if (pathname === '/wiki/Main_Page') {
-    pageName = 'Main Page'
-    detailsText = 'Browsing Main Page'
-  } else if (pathname.startsWith('/wiki/User:')) {
-    const parts = pathname.split('/')
-    const username =
-      parts.length >= 3 && parts[2]
-        ? decodeURIComponent(parts[2].replace(/_/g, ' '))
-        : 'Unknown User'
-    pageName = username
-    detailsText = `Viewing user "${username}"'s page`
-  } else if (pathname.startsWith('/w/index.php') && search.includes('action=history')) {
-    const params = new URLSearchParams(search)
-    const titleParam = params.get('title')
-    const title = titleParam
-      ? decodeURIComponent(titleParam.replace(/_/g, ' '))
-      : 'Unknown Page'
-    pageName = title
-    detailsText = `Viewing revision history of "${title}"`
-  } else if (pathname.startsWith('/wiki/')) {
-    const parts = pathname.split('/')
-    const article =
-      parts.length >= 3 && parts[2]
-        ? decodeURIComponent(parts[2].replace(/_/g, ' '))
-        : 'Unknown Article'
-    pageName = article
-    detailsText = 'Reading article'
-  }
-
-  const presenceData = {
-    largeImageKey: 'https://i.imgur.com/8BOtvbS.jpeg',
-    details: detailsText,
-    state: pageName,
-    startTimestamp: browsingTimestamp,
-  }
-
-  const pmid = (window as any)?.PMID
-  if (pmid && typeof pmid.setActivity === 'function') {
-    pmid.setActivity(presenceData)
-  }
+enum ActivityAssets {
+  Logo = "logo",
 }
 
-updatePresence()
-window.addEventListener('popstate', updatePresence)
-window.addEventListener('pushstate', updatePresence)
+presence.on("UpdateData", async () => {
+  const { pathname } = document.location;
+
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: Math.floor(Date.now() / 1000),
+    details: "Browsing PsychonautWiki",
+    state: "",
+  };
+
+  if (pathname === "/wiki/Main_Page") {
+    presenceData.state = "Browsing the Main Page";
+  } else if (pathname.startsWith("/wiki/")) {
+    const articleName = decodeURIComponent(
+      pathname.replace("/wiki/", "").replace(/_/g, " ")
+    );
+    presenceData.state = `Browsing the article "${articleName}"`;
+  } else {
+    presenceData.state = "Browsing the Wiki";
+  }
+
+  presence.setActivity(presenceData);
+});
