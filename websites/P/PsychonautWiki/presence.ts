18d41e58693b32a74513f60f9fe200eb75461f2c
Zinibow <ghostyguy67@gmail.com>
2026-01-06 23:45:47 -0800

    Fix ESLints Dumb Errors V2

--- websites/P/PsychonautWiki/presence.ts
+++ websites/P/PsychonautWiki/presence.ts
@@ -1,24 +1,26 @@
-const browsingTimestamp = Math.floor(Date.now() / 1000)
+const browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const presence = new Presence({
-  clientId: '1432152324163502130',
-})
+  clientId: "1432152324163502130",
+});
 
-presence.on('UpdateData', async () => {
-  if (document.location.hostname !== 'psychonautwiki.org') return
+presence.on("UpdateData", async () => {
+  if (document.location.hostname !== "psychonautwiki.org") return;
 
-  const pathname = document.location.pathname
+  const pathname = document.location.pathname;
 
   const presenceData: PresenceData = {
-    largeImageKey: 'https://i.imgur.com/GPxHYOV.png',
+    largeImageKey: "https://i.imgur.com/GPxHYOV.png",
     startTimestamp: browsingTimestamp,
     details:
-      pathname === '/wiki/Main_Page'
-          ? 'Browsing the Main Page'
-          : pathname.startsWith('/wiki/')
-          ? `Browsing the article "${decodeURIComponent(pathname.replace('/wiki/', '').replace(/_/g, ' '))}"`
-          : 'Browsing the Wiki',
-  }
+      pathname === "/wiki/Main_Page"
+        ? "Browsing the Main Page"
+        : pathname.startsWith("/wiki/")
+          ? `Browsing the article "${decodeURIComponent(
+              pathname.replace("/wiki/", "").replace(/_/g, " "),
+            )}"`
+          : "Browsing the Wiki",
+  };
 
-  presence.setActivity(presenceData)
-})
+  presence.setActivity(presenceData);
+});
