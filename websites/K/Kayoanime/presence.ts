dcf3a439c746e32dc4b29f4b554fc0a07a28eafe
Zinibow <ghostyguy67@gmail.com>
2026-01-08 14:31:21 -0800

    Did Recommended Changes

--- websites/K/Kayoanime/presence.ts
+++ websites/K/Kayoanime/presence.ts
@@ -28,6 +28,7 @@ presence.on(
 )
 presence.on('UpdateData', async () => {
   const fetch_img = document.querySelector<HTMLMetaElement>('meta[property="og:image"]')
+
     ?.content
 
   const presenceData: PresenceData = {
@@ -57,6 +58,7 @@ presence.on('UpdateData', async () => {
 
     default:
       if (document.querySelector('.entry-header-outer > .entry-header > h1 ')) {
+        
         if (fetch_img) {
           presenceData.largeImageKey = fetch_img
         }
