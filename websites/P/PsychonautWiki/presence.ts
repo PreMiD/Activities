879003f2e004ff2618c74315467a162e116d2687
Zinibow <ghostyguy67@gmail.com>
2026-01-07 20:30:03 -0800

    Fixed ESLint V2

--- websites/P/PsychonautWiki/presence.ts
+++ websites/P/PsychonautWiki/presence.ts
@@ -1,47 +1,52 @@
-const browsingTimestamp = Math.floor(Date.now() / 1000);
+const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 const presence = new Presence({
-  clientId: "1432152324163502130",
-});
+  clientId: '1432152324163502130',
+})
 
-presence.on("UpdateData", async () => {
-  if (document.location.hostname !== "psychonautwiki.org") return;
+presence.on('UpdateData', async () => {
+  if (document.location.hostname !== 'psychonautwiki.org')
+    return
 
-  const { pathname, search } = document.location;
-  const params = new URLSearchParams(search);
+  const { pathname, search } = document.location
+  const params = new URLSearchParams(search)
 
-  let articleName: string | null = null;
+  let articleName: string | null = null
 
-  if (pathname.startsWith("/w/index.php") && params.has("title")) {
-    articleName = decodeURIComponent(params.get("title")!)
-      .replace(/_/g, " ")
-      .trim();
-  } else if (pathname.startsWith("/wiki/")) {
-    articleName = decodeURIComponent(pathname.replace("/wiki/", ""))
-      .replace(/_/g, " ")
-      .trim();
+  if (pathname.startsWith('/w/index.php') && params.has('title')) {
+    articleName = decodeURIComponent(params.get('title')!)
+      .replace(/_/g, ' ')
+      .trim()
+  }
+  else if (pathname.startsWith('/wiki/')) {
+    articleName = decodeURIComponent(pathname.replace('/wiki/', ''))
+      .replace(/_/g, ' ')
+      .trim()
   }
 
-  const action = params.get("action");
-  const isEditing =
-    params.has("veaction") || action === "edit" || action === "submit";
+  const action = params.get('action')
+  const isEditing
+    = params.has('veaction') || action === 'edit' || action === 'submit'
 
   const presenceData: PresenceData = {
-    largeImageKey: "https://i.imgur.com/GPxHYOV.png",
+    largeImageKey: 'https://i.imgur.com/GPxHYOV.png',
     startTimestamp: browsingTimestamp,
-  };
+  }
 
   if (isEditing) {
     presenceData.details = articleName
       ? `Editing article "${articleName}"`
-      : "Editing article";
-  } else if (pathname === "/wiki/Main_Page") {
-    presenceData.details = "Browsing the Main Page";
-  } else if (articleName) {
-    presenceData.details = `Browsing the article "${articleName}"`;
-  } else {
-    presenceData.details = "Browsing the Wiki";
+      : 'Editing article'
+  }
+  else if (pathname === '/wiki/Main_Page') {
+    presenceData.details = 'Browsing the Main Page'
+  }
+  else if (articleName) {
+    presenceData.details = `Browsing the article "${articleName}"`
+  }
+  else {
+    presenceData.details = 'Browsing the Wiki'
   }
 
-  presence.setActivity(presenceData);
-});
+  presence.setActivity(presenceData)
+})
