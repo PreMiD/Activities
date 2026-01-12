79cd14ed07e49eea1ebe0534c32dbed0a211eb77
Nam Anh <phamnamanh25@gmail.com>
2026-01-13 01:30:53 +0700

    feat: add option to change status display text

--- websites/A/Apple Music/presence.ts
+++ websites/A/Apple Music/presence.ts
@@ -1,4 +1,4 @@
-import { ActivityType, Assets, getTimestamps } from 'premid'
+import { ActivityType, Assets, getTimestamps, StatusDisplayType } from 'premid'
 
 interface Attributes {
   albumName: string
@@ -25,6 +25,17 @@ interface AudioPlayer {
   _playbackDidStart: boolean
 }
 
+interface Metadata {
+  name: string
+  artist: string
+  album: string
+  artwork: string
+  paused: boolean
+  elapsedTime: number
+  duration: number
+  url?: string
+}
+
 const presence = new Presence({
   clientId: '842112189618978897',
 })
@@ -46,61 +57,99 @@ presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
     largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/A/Apple%20Music/assets/logo.png',
   } as PresenceData
-  const [hidePaused, timestamps, cover, playback, buttons, listening] = await Promise.all([
+  const [hidePaused, displayType, timestamps, cover, playback, buttons, listening] = await Promise.all([
     presence.getSetting<boolean>('hidePaused'),
+    presence.getSetting<number>('displayType'),
     presence.getSetting<boolean>('timestamps'),
     presence.getSetting<boolean>('cover'),
     presence.getSetting<boolean>('playback'),
     presence.getSetting<boolean>('buttons'),
     presence.getSetting<boolean>('listening'),
   ])
-  const audio = document.querySelector<HTMLAudioElement>(
-    'audio#apple-music-player',
-  )
-  const video = document
-    .querySelector('apple-music-video-player')
-    ?.shadowRoot
-    ?.querySelector(
-      'amp-window-takeover > .container > amp-video-player-internal',
+
+  async function getData(): Promise<Metadata | null> {
+    const audio = document.querySelector<HTMLAudioElement>(
+      'audio#apple-music-player',
     )
-    ?.shadowRoot
-    ?.querySelector('amp-video-player')
-    ?.shadowRoot
-    ?.querySelector('div#video-container')
-    ?.querySelector<HTMLVideoElement>('video#apple-music-video-player')
+    const video = document
+      .querySelector('apple-music-video-player')
+      ?.shadowRoot
+      ?.querySelector(
+        'amp-window-takeover > .container > amp-video-player-internal',
+      )
+      ?.shadowRoot
+      ?.querySelector('amp-video-player')
+      ?.shadowRoot
+      ?.querySelector('div#video-container')
+      ?.querySelector<HTMLVideoElement>('video#apple-music-video-player')
 
-  const audioPlayer = await presence.getPageVariable<{ audioPlayer: AudioPlayer }>('audioPlayer').then(res => res?.audioPlayer)
-  if (audioPlayer?._nowPlayingItem) {
-    const { attributes } = audioPlayer._nowPlayingItem
-    const paused = audioPlayer._paused || audioPlayer._stopped || !audioPlayer._playbackDidStart
+    const audioPlayer = await presence.getPageVariable<{ audioPlayer: AudioPlayer }>('audioPlayer').then(res => res?.audioPlayer)
 
-    if (paused && hidePaused) {
+    if (audioPlayer?._nowPlayingItem) {
+      const paused = audioPlayer._paused || audioPlayer._stopped || !audioPlayer._playbackDidStart
+      const { attributes } = audioPlayer._nowPlayingItem
+      const { artwork, name, artistName, albumName, durationInMillis, url } = attributes
+      const duration = durationInMillis / 1000
+
+      return {
+        album: albumName,
+        artist: artistName,
+        artwork: artwork.url.replace('{w}', String(artwork.width)).replace('{h}', String(artwork.height)),
+        duration,
+        elapsedTime: audioPlayer._currentPlaybackProgress * duration,
+        name,
+        paused,
+        url,
+      }
+    }
+    else if (video?.title || audio?.title) {
+      const media = video || audio
+      const paused = !!media && (media.paused || media.readyState <= 2)
+      const metadata = navigator.mediaSession.metadata
+
+      return {
+        album: metadata?.album || '',
+        artist: metadata?.artist || '',
+        artwork: metadata?.artwork[0]?.src.replace(/\d{1,2}x\d{1,2}[a-z]{1,2}/, '1024x1024') || '',
+        duration: media!.duration,
+        elapsedTime: media!.currentTime,
+        name: metadata?.title || '',
+        paused,
+        url: undefined,
+      }
+    }
+
+    return null
+  }
+
+  const data = await getData()
+  if (data) {
+    presenceData.details = data.name
+    presenceData.state = data.artist
+
+    if (data.paused && hidePaused) {
       clearActivity()
       return
     }
 
-    presenceData.details = attributes.name
-    presenceData.state = attributes.artistName
-
     if (playback) {
-      presenceData.smallImageKey = paused ? Assets.Pause : Assets.Play
-      presenceData.smallImageText = paused
+      presenceData.smallImageKey = data.paused ? Assets.Pause : Assets.Play
+      presenceData.smallImageText = data.paused
         ? (await strings).pause
         : (await strings).play
     }
 
     if (cover) {
-      const { url, width, height } = attributes.artwork
-      presenceData.largeImageKey = url.replace('{w}', String(width)).replace('{h}', String(height))
-      presenceData.largeImageText = attributes.albumName
+      presenceData.largeImageKey = data.artwork
+      presenceData.largeImageText = data.album
     }
 
     [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(
-      audioPlayer._currentPlaybackProgress * attributes.durationInMillis / 1000 || 0,
-      attributes.durationInMillis / 1000,
+      data.elapsedTime,
+      data.duration,
     )
 
-    if (paused || !timestamps) {
+    if (data.paused || !timestamps) {
       delete presenceData.startTimestamp
       delete presenceData.endTimestamp
     }
@@ -108,62 +157,23 @@ presence.on('UpdateData', async () => {
     if (listening)
       presenceData.type = ActivityType.Listening
 
-    if (buttons && attributes.url) {
+    if (buttons && data.url) {
       presenceData.buttons = [
         {
           label: 'Listen on Apple Music',
-          url: attributes.url,
+          url: data.url,
         },
       ]
     }
 
-    presence.setActivity(presenceData)
-  }
-  else if (video?.title || audio?.title) {
-    const media = video || audio
-    const timestamp = document
-      .querySelector('amp-lcd.lcd.lcd__music')
-      ?.shadowRoot
-      ?.querySelector<HTMLInputElement>(
-        'input#playback-progress[aria-valuenow][aria-valuemax]',
-      )
-    const paused = media && (media.paused || media.readyState <= 2)
-
-    if (paused && hidePaused) {
-      clearActivity()
-      return
+    switch (displayType) {
+      case 1:
+        presenceData.statusDisplayType = StatusDisplayType.State
+        break
+      case 2:
+        presenceData.statusDisplayType = StatusDisplayType.Details
+        break
     }
-
-    presenceData.details = navigator.mediaSession.metadata?.title
-    presenceData.state = navigator.mediaSession.metadata?.artist
-
-    if (playback) {
-      presenceData.smallImageKey = paused ? Assets.Pause : Assets.Play
-      presenceData.smallImageText = paused
-        ? (await strings).pause
-        : (await strings).play
-    }
-
-    if (cover) {
-      presenceData.largeImageKey = navigator.mediaSession.metadata?.artwork[0]?.src.replace(
-        /\d{1,2}x\d{1,2}[a-z]{1,2}/,
-        '1024x1024',
-      )
-      presenceData.largeImageText = navigator.mediaSession.metadata?.album
-    }
-
-    [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(
-      Number(timestamp ? timestamp.ariaValueNow : media!.currentTime),
-      Number(timestamp ? timestamp.ariaValueMax : media!.duration),
-    )
-
-    if (paused || !timestamps) {
-      delete presenceData.startTimestamp
-      delete presenceData.endTimestamp
-    }
-
-    if (listening)
-      presenceData.type = ActivityType.Listening
 
     presence.setActivity(presenceData)
   }
