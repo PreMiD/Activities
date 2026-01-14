de81c13ab5fa7b59457521a33b25b8b877c748f5
Logan <95306463+LaganYT@users.noreply.github.com>
2026-01-13 18:42:11 -0600

    Update BandLab metadata and presence logic
    - Added a regular expression for BandLab URLs in metadata.json.
    - Reformatted JSON structure for consistency.
    - Enhanced presence.ts to improve activity tracking and user experience.
    
    Signed-off-by: Logan <95306463+LaganYT@users.noreply.github.com>

--- websites/B/BandLab/presence.ts
+++ websites/B/BandLab/presence.ts
@@ -1,240 +1,243 @@
 import { ActivityType, Assets } from 'premid'
 
 const presence = new Presence({
-	clientId: '801743263052726292',
+  clientId: '801743263052726292',
 })
 
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 let studioTimestamp = 0
 
 enum ActivityAssets {
-	Logo = 'https://i.imgur.com/xTXlFCo.png',
+  Logo = 'https://i.imgur.com/xTXlFCo.png',
 }
 
 presence.on('UpdateData', async () => {
-	const [showButtons, privacyMode, privacyModeStrict, titleArtistFormat]
-		= await Promise.all([
-			presence.getSetting<boolean>('showButtons'),
-			presence.getSetting<boolean>('privacyMode'),
-			presence.getSetting<boolean>('privacyModeStrict'),
-			presence.getSetting<string>('titleArtistFormat').then(str => str || '%title% - %artist%'),
-		])
+  const [showButtons, privacyMode, privacyModeStrict, titleArtistFormat]
+    = await Promise.all([
+      presence.getSetting<boolean>('showButtons'),
+      presence.getSetting<boolean>('privacyMode'),
+      presence.getSetting<boolean>('privacyModeStrict'),
+      presence.getSetting<string>('titleArtistFormat').then(str => str || '%title% - %artist%'),
+    ])
 
-	const strings = await presence.getStrings({
-		play: 'general.playing',
-		pause: 'general.paused',
-		browse: 'general.browsing',
-		view: 'general.view',
-		buttonViewSong: 'general.buttonViewSong',
-		listening: 'general.listeningTo',
-		listening_unspecified: 'general.listeningMusic',
-		viewUser: 'general.viewUser',
-		readingADM: 'general.readingADM',
-		readingDM: 'general.readingDM',
-		buttonViewPage: 'general.buttonViewPage',
-		watching: 'general.watching',
-		buttonWatchVideo: 'general.buttonWatchVideo',
-		videoTarget: 'bandlab.videoTarget',
-		searchUnspecified: 'general. searchSomething',
-		search: 'general.searchFor',
-		searchHint: 'general.search',
-	})
+  const strings = await presence.getStrings({
+    play: 'general.playing',
+    pause: 'general.paused',
+    browse: 'general.browsing',
+    view: 'general.view',
+    buttonViewSong: 'general.buttonViewSong',
+    listening: 'general.listeningTo',
+    listening_unspecified: 'general.listeningMusic',
+    viewUser: 'general.viewUser',
+    readingADM: 'general.readingADM',
+    readingDM: 'general.readingDM',
+    buttonViewPage: 'general.buttonViewPage',
+    watching: 'general.watching',
+    buttonWatchVideo: 'general.buttonWatchVideo',
+    videoTarget: 'bandlab.videoTarget',
+    searchUnspecified: 'general. searchSomething',
+    search: 'general.searchFor',
+    searchHint: 'general.search',
+  })
 
-	function firstSrcFromSrcsetProvided(e: Element | null): string | undefined {
-		return e === null ? ActivityAssets.Logo : e.getAttribute('srcset')?.split(' ')[0]
-	}
+  function firstSrcFromSrcsetProvided(e: Element | null): string | undefined {
+    return e === null ? ActivityAssets.Logo : e.getAttribute('srcset')?.split(' ')[0]
+  }
 
-	const { pathname, search, href } = document.location
+  const { pathname, search, href } = document.location
 
-	const presenceData: PresenceData = {
-		largeImageKey: ActivityAssets. Logo,
-		startTimestamp: studioTimestamp !== 0 ? studioTimestamp : browsingTimestamp,
-		type: ActivityType.Playing,
-	}
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets. Logo,
+    startTimestamp: studioTimestamp !== 0 ? studioTimestamp : browsingTimestamp,
+    type: ActivityType.Playing,
+  }
 
-	if (pathname.startsWith('/feed/')) {
-		presenceData.details = strings.browse
+  if (pathname.startsWith('/feed/')) {
+    presenceData.details = strings.browse
 
-		if (pathname.includes('trending'))
-			presenceData.state = 'Looking at what\'s trending'
+    if (pathname.includes('trending')) {
+      presenceData.state = 'Looking at what\'s trending'
+    }
+    else if (pathname.includes('following')) {
+      presenceData.state = 'Catching up with the people they follow'
+    }
+    else if (pathname.includes('for-you')) {
+      presenceData.state = 'The For You page'
+    }
 
-		else if (pathname.includes('following'))
-			presenceData.state = 'Catching up with the people they follow'
+    else if (pathname. includes('video')) {
+      if (search.trim() === '') {
+        presenceData.state = 'Looking at popular videos'
+      }
 
-		else if (pathname.includes('for-you'))
-			presenceData.state = 'The For You page'
+      else if (search.includes('postId')) {
+        presenceData.details = strings.watching
 
-		else if (pathname. includes('video')) {
-			if (search.trim() === '')
-				presenceData.state = 'Looking at popular videos'
+        const videoAuthor:  {
+          element: Element | null
+          target: string
+          username: string
+        } = {
+          element: null,
+          target: '',
+          username: '',
+        }
 
-			else if (search.includes('postId')) {
-				presenceData.details = strings.watching
+        const homeElement: Element | null = document.querySelector('post-card-header-content > div > . post-card-header-title > a')
 
-				const videoAuthor:  {
-					element: Element | null
-					target: string
-					username: string
-				} = {
-					element: null,
-					target: '',
-					username: '',
-				}
+        if (homeElement) {
+          videoAuthor.element = homeElement
+          videoAuthor.target = homeElement?. getAttribute('href') || ''
+          videoAuthor.username = homeElement?.textContent || 'a BandLab user'
+        }
 
-				const homeElement: Element | null = document.querySelector('post-card-header-content > div > . post-card-header-title > a')
+        if (showButtons) {
+          presenceData.buttons = [
+            {
+              label: strings.buttonWatchVideo,
+              url: href,
+            },
+            {
+              label: strings.viewUser,
+              url: videoAuthor.target,
+            },
+          ]
+        }
 
-				if (homeElement) {
-					videoAuthor.element = homeElement
-					videoAuthor.target = homeElement?. getAttribute('href') || ''
-					videoAuthor.username = homeElement?.textContent || 'a BandLab user'
-				}
+        presenceData.state = 'A video by [1]'.replace('[1]', videoAuthor. username)
+      }
+    }
+    else if (pathname.includes('communities')) {
+      presenceData.state = 'Looking for communities'
+    }
+  }
 
-				if (showButtons) {
-					presenceData.buttons = [
-						{
-							label: strings.buttonWatchVideo,
-							url: href,
-						},
-						{
-							label: strings.viewUser,
-							url: videoAuthor.target,
-						},
-					]
-				}
+  if (pathname. startsWith('/library/projects/') && !pathname.includes('likes'))
+    presenceData.state = 'Looking at their saved projects'
 
-				presenceData.state = 'A video by [1]'.replace('[1]', videoAuthor. username)
-			}
-		}
-		else if (pathname.includes('communities'))
-			presenceData.state = 'Looking for communities'
-	}
+  if (pathname. startsWith('/track/')) {
+    studioTimestamp = 0
 
-	if (pathname. startsWith('/library/projects/') && !pathname.includes('likes'))
-		presenceData.state = 'Looking at their saved projects'
+    const isPublic:  boolean = document.querySelector('post-private-badge') === null
 
-	if (pathname. startsWith('/track/')) {
-		studioTimestamp = 0
+    if (! isPublic && privacyMode) {
+      presenceData.state = strings.listening_unspecified
+    }
+    else {
+      const title: string = document.querySelector('. track-page-player-title-name')?.textContent?. trim() || 'A song'
+      const artist: string = document.querySelector('a.track-card-subtitle-text')?.textContent?.trim() || 'someone'
 
-		const isPublic:  boolean = document.querySelector('post-private-badge') === null
+      presenceData.details = strings.view
+      presenceData.state = titleArtistFormat.replace('%title%', title).replace('%artist%', artist)
 
-		if (! isPublic && privacyMode) {
-			presenceData.state = strings.listening_unspecified
-		}
-		else {
-			const title: string = document.querySelector('. track-page-player-title-name')?.textContent?. trim() || 'A song'
-			const artist: string = document.querySelector('a.track-card-subtitle-text')?.textContent?.trim() || 'someone'
+      presenceData.largeImageKey = firstSrcFromSrcsetProvided(document.querySelector('img.ds-cover'))
+      presenceData.smallImageKey = ActivityAssets.Logo
+      presenceData.smallImageText = 'BandLab'
 
-			presenceData.details = strings.view
-			presenceData.state = titleArtistFormat.replace('%title%', title).replace('%artist%', artist)
+      if (isPublic && showButtons) {
+        presenceData.buttons = [
+          {
+            label: strings.buttonViewSong,
+            url: href,
+          },
+        ]
+      }
 
-			presenceData.largeImageKey = firstSrcFromSrcsetProvided(document.querySelector('img.ds-cover'))
-			presenceData.smallImageKey = ActivityAssets.Logo
-			presenceData.smallImageText = 'BandLab'
+      const bottomPlayButton: Element | null = document.querySelector('button.ds-play-button')
+      if (bottomPlayButton) {
+        if (bottomPlayButton.className. includes('status-paused')) {
+          presenceData.smallImageKey = Assets.Pause
+          presenceData.smallImageText = strings.pause
+        }
+        else if (bottomPlayButton.className.includes('status-playing')) {
+          presenceData.details = strings.listening. replace('{0}{1}', ':  ')
+          presenceData.smallImageKey = Assets.Play
+          presenceData.smallImageText = strings.play
+        }
+      }
+    }
+  }
 
-			if (isPublic && showButtons) {
-				presenceData.buttons = [
-					{
-						label: strings.buttonViewSong,
-						url: href,
-					},
-				]
-			}
+  if (pathname.includes('/studio')) {
+    presenceData.details = 'In the Studio'
 
-			const bottomPlayButton: Element | null = document.querySelector('button.ds-play-button')
-			if (bottomPlayButton) {
-				if (bottomPlayButton.className. includes('status-paused')) {
-					presenceData.smallImageKey = Assets.Pause
-					presenceData.smallImageText = strings.pause
-				}
-				else if (bottomPlayButton.className.includes('status-playing')) {
-					presenceData.details = strings.listening. replace('{0}{1}', ':  ')
-					presenceData.smallImageKey = Assets.Play
-					presenceData.smallImageText = strings.play
-				}
-			}
-		}
-	}
+    if (studioTimestamp === 0)
+      studioTimestamp = Math.floor(Date.now() / 1000)
 
-	if (pathname.includes('/studio')) {
-		presenceData.details = 'In the Studio'
+    if (privacyMode) {
+      presenceData.state = 'Cooking up some heat'
+    }
+    else {
+      const inputBox = document.getElementById('studio-project-name-input') as HTMLInputElement
+      const title:  string = inputBox?.value. trim() || 'New Project'
+      presenceData.state = 'Working on project:  {0}'.replace('{0}', title)
+    }
+  }
 
-		if (studioTimestamp === 0)
-			studioTimestamp = Math.floor(Date.now() / 1000)
+  if (pathname.startsWith('/chat')) {
+    if (privacyModeStrict) {
+      presenceData.state = strings.readingADM
+    }
+    else if (!privacyModeStrict && document.querySelector('header.conversation-partner')) {
+      const dmRecipientName: string | null | undefined = document.querySelector('. profile-tile-title > a')?.textContent
+      const dmSubtitleName: string | null | undefined = document.querySelector('.profile-tile-subtitle-wrap > ng-pluralize')?.textContent || document.querySelector('.profile-tile-subtitle-wrap > a')?.textContent || 'a BandLab user'
 
-		if (privacyMode) {
-			presenceData.state = 'Cooking up some heat'
-		}
-		else {
-			const inputBox = document.getElementById('studio-project-name-input') as HTMLInputElement
-			const title:  string = inputBox?.value. trim() || 'New Project'
-			presenceData.state = 'Working on project:  {0}'.replace('{0}', title)
-		}
-	}
+      presenceData.details = strings.readingDM
+      presenceData.state = `${dmRecipientName} (${dmSubtitleName})`
 
-	if (pathname.startsWith('/chat')) {
-		if (privacyModeStrict) {
-			presenceData.state = strings.readingADM
-		}
-		else if (!privacyModeStrict && document.querySelector('header.conversation-partner')) {
-			const dmRecipientName: string | null | undefined = document.querySelector('. profile-tile-title > a')?.textContent
-			const dmSubtitleName: string | null | undefined = document.querySelector('.profile-tile-subtitle-wrap > ng-pluralize')?.textContent || document.querySelector('.profile-tile-subtitle-wrap > a')?.textContent || 'a BandLab user'
+      presenceData.largeImageKey = firstSrcFromSrcsetProvided(document.querySelector('header.profile-tile-header > a > img. profile-tile-picture') || document.querySelector('a.profile-tile-header > img'))
 
-			presenceData.details = strings.readingDM
-			presenceData.state = `${dmRecipientName} (${dmSubtitleName})`
+      presenceData.smallImageKey = ActivityAssets.Logo
+      presenceData.smallImageText = 'BandLab'
+    }
+    else {
+      presenceData. state = strings.readingADM
+    }
+  }
 
-			presenceData.largeImageKey = firstSrcFromSrcsetProvided(document.querySelector('header.profile-tile-header > a > img. profile-tile-picture') || document.querySelector('a.profile-tile-header > img'))
+  if (pathname.startsWith('/search/')) {
+    presenceData.smallImageKey = Assets.Search
+    presenceData. smallImageText = strings.searchHint
 
-			presenceData.smallImageKey = ActivityAssets.Logo
-			presenceData.smallImageText = 'BandLab'
-		}
-		else {
-			presenceData. state = strings.readingADM
-		}
-	}
+    if (privacyModeStrict) {
+      presenceData.state = strings.searchUnspecified
+    }
+    else {
+      presenceData. details = strings.search
+      presenceData.state = new URLSearchParams(document.location.search).get('q')
+    }
+  }
 
-	if (pathname.startsWith('/search/')) {
-		presenceData.smallImageKey = Assets.Search
-		presenceData. smallImageText = strings.searchHint
+  if (document.querySelector('.profile-card-title')) {
+    presenceData.details = pathname.startsWith('/band/') ? 'Viewing band: ' : strings.viewUser
+    presenceData.buttons = [
+      {
+        label: strings.buttonViewPage,
+        url: href,
+      },
+    ]
 
-		if (privacyModeStrict) {
-			presenceData.state = strings.searchUnspecified
-		}
-		else {
-			presenceData. details = strings.search
-			presenceData.state = new URLSearchParams(document.location.search).get('q')
-		}
-	}
+    const pfp: string | undefined = firstSrcFromSrcsetProvided(document.querySelector('.profile-card-picture > a > img'))
 
-	if (document.querySelector('.profile-card-title')) {
-		presenceData.details = pathname.startsWith('/band/') ? 'Viewing band: ' : strings.viewUser
-		presenceData.buttons = [
-			{
-				label: strings.buttonViewPage,
-				url: href,
-			},
-		]
+    if (pfp !== ActivityAssets.Logo) {
+      presenceData.largeImageKey = pfp
+      presenceData.smallImageKey = ActivityAssets.Logo
+      presenceData. smallImageText = 'BandLab'
+    }
+    else {
+      presenceData. largeImageKey = ActivityAssets.Logo
+    }
 
-		const pfp: string | undefined = firstSrcFromSrcsetProvided(document.querySelector('.profile-card-picture > a > img'))
+    presenceData.state = `${document.querySelector('.profile-card-title')?.textContent?.trim()}${
+      pathname.startsWith('/band/')
+        ? ''
+        : ` (${document.querySelector('.profile-card-subtitle > a')?.textContent?.trim()})`
+    }`
+  }
 
-		if (pfp !== ActivityAssets.Logo) {
-			presenceData.largeImageKey = pfp
-			presenceData.smallImageKey = ActivityAssets.Logo
-			presenceData. smallImageText = 'BandLab'
-		}
-		else {
-			presenceData. largeImageKey = ActivityAssets.Logo
-		}
+  if (presenceData.state)
+    presence.setActivity(presenceData)
 
-		presenceData.state = `${document.querySelector('.profile-card-title')?.textContent?.trim()}${
-			pathname.startsWith('/band/')
-				? ''
-				: ` (${document.querySelector('.profile-card-subtitle > a')?.textContent?.trim()})`
-		}`
-	}
-
-	if (presenceData.state)
-		presence.setActivity(presenceData)
-
-	else
-		presence.clearActivity()
+  else
+    presence.clearActivity()
 })
