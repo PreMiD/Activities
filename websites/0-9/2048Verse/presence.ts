3d46d3f2292cd0e04837bf70468083afbd232cea
Narwhal <github@narwhalkid.com>
2026-03-08 18:52:31 -0400

    Update for new front end

--- websites/0-9/2048Verse/presence.ts
+++ websites/0-9/2048Verse/presence.ts
@@ -1,81 +1,82 @@
 const presence = new Presence({
   clientId: '939893158777618432',
-})
-const browsingTimestamp = Math.floor(Date.now() / 1000)
+});
+const browsingTimestamp = Math.floor(Date.now() / 1000);
 
 enum ActivityAssets {
   Logo = 'https://cdn.rcd.gg/PreMiD/websites/0-9/2048Verse/assets/logo.png',
-}
+};
 
 presence.on('UpdateData', async () => {
   const [time, buttons] = await Promise.all([
     presence.getSetting<boolean>('time'),
     presence.getSetting<boolean>('buttons'),
-  ])
+  ]);
   const presenceData: PresenceData = {
     largeImageKey: ActivityAssets.Logo,
     startTimestamp: browsingTimestamp,
+  };
+  const { pathname, href } = document.location;
+  const board = document.querySelector('.board');
+  let gridSize = "";
+  if (board) {
+    const computedStyle = window.getComputedStyle(board);
+    gridSize = `${computedStyle.getPropertyValue('--board-height')}x${
+      computedStyle.getPropertyValue('--board-width')}`;
   }
-  const { pathname, href } = document.location
-  const gridSize = `${document.querySelectorAll('.grid-row').length.toString()}x${(
-    document.querySelectorAll('.grid-cell').length
-    / document.querySelectorAll('.grid-row').length
-  ).toString()}`
-
   if (pathname.split('/')[1] === 'leaderboard') {
-    let leaderboardType
-    switch (document.querySelector<HTMLDivElement>('#marker')?.style.left) {
-      case '-280px': {
-        leaderboardType = 'All Time'
-        break
+    let leaderboardType;
+    const variant = pathname.split('/')[2];
+    switch (pathname.split('/')[3]) {
+      case 'all': {
+        leaderboardType = 'All Time';
+        break;
       }
-      case '-157px': {
-        leaderboardType = 'Today'
-        break
+      case 'day': {
+        leaderboardType = 'Today';
+        break;
       }
-      case '-44px': {
-        leaderboardType = 'Week'
-        break
+      case 'week': {
+        leaderboardType = 'Week';
+        break;
       }
     }
-    presenceData.details = 'Viewing leaderboard'
-    presenceData.state = `${
-      document.querySelector<HTMLSelectElement>('#variant')?.value
-    } ${leaderboardType}`
+    presenceData.details = 'Viewing leaderboard';
+    presenceData.state = `${variant} ${leaderboardType}`;
   }
   else if (pathname.split('/')[1] === 'user') {
-    presenceData.details = 'Viewing profile'
-    presenceData.state = document.querySelector('h1.title')?.firstChild?.textContent
-    presenceData.buttons = [{ label: 'View Profile', url: href }]
+    presenceData.details = 'Viewing profile';
+    presenceData.state = pathname.split('/')[2];
+    presenceData.buttons = [{ label: 'View Profile', url: href }];
   }
   else if (pathname.split('/')[1] === 'replay') {
-    presenceData.details = `Watching replay ${gridSize}`
+    presenceData.details = `Watching replay ${gridSize}`;
     presenceData.state = `Score ${
-      document.querySelector('.score-container')?.firstChild?.textContent
-    }`
+      document.querySelector('#SCORE .info-value')?.textContent
+    }`;
   }
   else if (pathname.split('/')[1] === 'p') {
-    presenceData.details = `Practising ${gridSize}`
-    presenceData.state = `Score ${
-      document.querySelector('.score-container')?.firstChild?.textContent
-    } Best ${document.querySelector('.best-container')?.textContent}`
+    presenceData.details = `Practicing ${gridSize}`;
   }
   else if (pathname.split('/')[1] === 'lessons') {
-    presenceData.details = 'Browsing lessons'
+    presenceData.details = 'Browsing lessons';
   }
-  else if (document.querySelectorAll('.grid-row').length) {
-    presenceData.details = `Playing ${gridSize}`
+  else if (pathname.split('/')[1] === 'settings') {
+    presenceData.details = 'Changing settings';
+  }
+  else if (board) {
+    presenceData.details = `Playing ${gridSize}`;
 
     presenceData.state = `Score ${
-      document.querySelector('.score-container')?.firstChild?.textContent
-    } Best ${document.querySelector('.best-container')?.textContent}`
+        document.querySelector('#SCORE .info-value')?.textContent
+    } Best ${document.querySelector('#BEST .info-value')?.textContent}`;
   }
 
   if (!time)
-    delete presenceData.startTimestamp
+    delete presenceData.startTimestamp;
 
   if (!buttons && presenceData.buttons)
-    delete presenceData.buttons
+    delete presenceData.buttons;
 
-  presence.setActivity(presenceData)
-})
+  presence.setActivity(presenceData);
+});
