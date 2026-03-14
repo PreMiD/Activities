e5df46e24250be484287cd8a1931a560e9882165
BartuAbiHD <bartucivas@gmail.com>
2026-03-14 12:30:16 +0300

    feat(video vitrini): The requested changes have been made.

--- websites/V/Video Vitrini/presence.ts
+++ websites/V/Video Vitrini/presence.ts
@@ -44,7 +44,7 @@ presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
     details: sayfaBasligi,
     state: 'Sitede geziniyor',
-    largeImageKey: 'https://i.ibb.co/hRQP8jH6/videovitrini.png',
+    largeImageKey: 'https://i.ibb.co/WvyTVytv/videovitrini.png',
     largeImageText: 'Video Vitrini',
     type: ActivityType.Watching,
     // Discord durumuna buton ekleme kısmı
