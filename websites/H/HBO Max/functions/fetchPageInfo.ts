dd3ec13e5de7585aeffe73ae0258449d043c974b
colourfulplanets <themrmonkeyman1@gmail.com>
2026-03-31 17:19:44 +1100

    fix(HBO Max): restructure codebase and improve presence data

---
+++ websites/H/HBO Max/functions/fetchPageInfo.ts
@@ -0,0 +1,38 @@
+import type { IncludedItem, MaxAPIResponse } from '../types.ts'
+import pLimit from 'p-limit'
+
+const limit = pLimit(1)
+
+// eslint-disable-next-line import/no-mutable-exports
+export let pageInfo: {
+  url: string
+  data?: MaxAPIResponse
+} | null = null
+
+export async function fetchPageInfo(pathname: string): Promise<void> {
+  await limit(async () => {
+    if (pageInfo?.url === document.location.href)
+      return
+
+    pageInfo = { url: document.location.href }
+
+    try {
+      pageInfo.data = await fetch(
+        `https://default.any-amer.prd.api.hbomax.com/cms/routes${pathname}?include=default&page[items.size]=10`,
+        {
+          method: 'GET',
+          credentials: 'include',
+        },
+      ).then(res => res.json())
+    }
+    catch {
+      pageInfo = null
+    }
+  })
+}
+
+export function clearPageInfo(): void {
+  pageInfo = null
+}
+
+export type { IncludedItem }
