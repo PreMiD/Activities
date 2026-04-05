a45be5c3d735044be0e16e05f327832097cc8e6c
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2026-04-04 23:28:29 -0300

    misc: move small logo url to enum

--- websites/C/Clearn/presence.ts
+++ websites/C/Clearn/presence.ts
@@ -7,13 +7,14 @@ const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
   Logo = 'https://imgur.com/vQmQSrT.png',
+  SmallLogo = 'https://imgur.com/03fGXs3.png'
 }
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
     largeImageKey: ActivityAssets.Logo,
     startTimestamp: browsingTimestamp,
-    smallImageKey: "https://imgur.com/03fGXs3.png",
+    smallImageKey: ActivityAssets.SmallLogo,
     details: "Coding on Clearn"
   }
 
