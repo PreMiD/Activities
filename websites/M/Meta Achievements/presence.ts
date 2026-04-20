fb82da29c5fd51a991e4196e9f409a8c29778db4
Andromeda <andromeda@notaglitch.net>
2026-04-20 12:38:00 -0400

    Reverting, no way forward with ESLint. Waiting for manual resolution

--- websites/M/Meta Achievements/presence.ts
+++ websites/M/Meta Achievements/presence.ts
@@ -41,12 +41,9 @@ presence.on('UpdateData', async () => {
     if (games || achievements || completions) {
       details = username ? `${username}'s Profile` : 'Loading Profile...'
       const parts = []
-      if
-      (games) parts.push(`${games} Games`)
-      if
-      (achievements) parts.push(`${achievements} Achievements`)
-      if
-      (completions) parts.push(`${completions} 100%`)
+      if (games) parts.push(`${games} Games`)
+      if (achievements) parts.push(`${achievements} Achievements`)
+      if (completions) parts.push(`${completions} 100%`)
 
       state = parts.join(' · ')
     }
@@ -55,8 +52,7 @@ presence.on('UpdateData', async () => {
     }
 
     const avatarImg = document.querySelector('#profileAvatar img') as HTMLImageElement
-    if
-    (avatarImg?.src) smallImageKey = avatarImg.src
+    if (avatarImg?.src) smallImageKey = avatarImg.src
   }
   else if (pathname.endsWith('achievementdefinitions.html')) {
     details = 'All Achievements'
@@ -67,11 +63,8 @@ presence.on('UpdateData', async () => {
 
     if (games || achievements) {
       const parts = []
-      if
-      (games) parts.push(`${games} Total Games`)
-
-      if
-      (achievements) parts.push(`${achievements} Achievements`)
+      if (games) parts.push(`${games} Total Games`)
+      if (achievements) parts.push(`${achievements} Achievements`)
 
       state = parts.join(' · ')
     }
