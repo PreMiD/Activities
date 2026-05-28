26c9b54fe6c44088a9a120ca0ddf7d47d15793f9
ikeblo <vitusousa22@gmail.com>
2026-05-28 00:19:04 -0300

    Update presence image URL in presence.ts
    Signed-off-by: ikeblo <vitusousa22@gmail.com>

--- websites/B/Basketball Reference/presence.ts
+++ websites/B/Basketball Reference/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/B/BasketballReference/assets/logo.png',
+    largeImageKey: ''https://i.imgur.com/7OIaOVU.png'',
     startTimestamp: browsingTimestamp,
   }
   const { pathname } = document.location
