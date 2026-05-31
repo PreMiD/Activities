26292cd53bc0921ce43a66ecc95b215bfd5ed4c4
Raghav <75294599+ragxxv@users.noreply.github.com>
2026-05-31 16:57:25 +0530

    JioHotstar Extension
    Signed-off-by: Raghav <75294599+ragxxv@users.noreply.github.com>

---
+++ websites/J/JioHotstar/presence.ts
@@ -0,0 +1,71 @@
+import { ActivityType, Assets } from 'premid'
+
+const presence = new Presence({
+	clientId: "1510563692952158248",
+});
+
+presence.on("UpdateData", async () => {
+
+  if (location.pathname === "/in/home") {
+	presence.setActivity({
+		type: ActivityType.Watching,
+		details: "Browsing",
+		state: "Home",
+		largeImageKey: "https://pbs.twimg.com/profile_images/1982734390191898624/wxawHKIF_400x400.jpg",
+		largeImageText: "JioHotstar"
+	});
+
+	return;
+}
+
+	const video = document.querySelector("video") as HTMLVideoElement | null;
+	if (!video) {
+		presence.clearActivity();
+		return;
+	}
+
+	const title =
+		document.querySelector("h1")?.textContent?.trim() ??
+		"JioHotstar";
+
+	const text = document.body.innerText;
+
+	const episode =
+		text.match(/S\d+\s*E\d+/i)?.[0];
+
+  const poster =
+	document.querySelector(
+		'meta[property="twitter:image"]'
+	)?.getAttribute("content");
+
+	const activity: PresenceData = {
+	type: 3 as any,
+	details: title,
+	state: episode ?? "Watching",
+	largeImageKey: poster || "JioHotstar",
+	largeImageText: title
+};
+
+	if (episode) {
+		activity.state = episode;
+	}
+
+  const startTimestamp =
+	Math.floor(Date.now() / 1000) - Math.floor(video.currentTime);
+
+  const endTimestamp =
+	startTimestamp + Math.floor(video.duration);
+
+	if (!video.paused) {
+		activity.startTimestamp = startTimestamp;
+		activity.endTimestamp = endTimestamp;
+
+		activity.smallImageKey = Assets.Play
+		activity.smallImageText = "Playing";
+	} else {
+		activity.smallImageKey = Assets.Pause
+		activity.smallImageText = "Paused";
+	}
+
+	presence.setActivity(activity);
+});
