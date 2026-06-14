c5748613559d25031c674d7d8fdebfd6fc0bb29f
n4kushi <162187191+n4kushi@users.noreply.github.com>
2026-06-14 23:34:35 +0300

    Update presence.ts
    Signed-off-by: n4kushi <162187191+n4kushi@users.noreply.github.com>

--- websites/H/hangar/presence.ts
+++ websites/H/hangar/presence.ts
@@ -1,5 +1,5 @@
 const presence = new Presence({
-  clientId: '1004301145348526090'
+  clientId: '1003332362823802949'
 })
 
 const startTimestamp = Math.floor(Date.now() / 1000)
@@ -8,8 +8,8 @@ presence.on('UpdateData', async () => {
   const title = document.title || 'Hangar'
   const path = window.location.pathname
 
-  const presenceData: PresenceData = {
-    type: 3,
+  let presenceData: PresenceData = {
+    type: ActivityType.Watching,
     largeImageKey: 'logo',
     largeImageText: 'usehangar.gg',
     startTimestamp,
@@ -20,23 +20,47 @@ presence.on('UpdateData', async () => {
   if (path === '/' || path === '/feed') {
     presenceData.details = 'Browsing the homepage'
     presenceData.state = 'Viewing the feed'
-  } else if (path.startsWith('/hub/')) {
+  }
+
+  else if (path.startsWith('/hub/')) {
     const hubName = title.split(' | ')[0] || 'A Hub'
 
-    presenceData.details = 'Viewing a Hub'
-    presenceData.state = hubName
-    presenceData.smallImageKey = 'logo'
-    presenceData.smallImageText = 'Hangar'
-  } else if (path.startsWith('/profile/')) {
+    presenceData = {
+      type: ActivityType.Watching,
+      largeImageKey: 'logo',
+      largeImageText: 'usehangar.gg',
+      startTimestamp,
+      details: 'Viewing a Hub',
+      state: hubName,
+      smallImageKey: 'logo',
+      smallImageText: 'Hangar'
+    }
+  }
+
+  else if (path.startsWith('/profile/')) {
     const user = title.split(' | ')[0] || 'A profile'
 
-    presenceData.details = 'Viewing a profile'
-    presenceData.state = user
-  } else if (path.startsWith('/post/')) {
+    presenceData = {
+      type: ActivityType.Watching,
+      largeImageKey: 'logo',
+      largeImageText: 'usehangar.gg',
+      startTimestamp,
+      details: 'Viewing a profile',
+      state: user
+    }
+  }
+
+  else if (path.startsWith('/post/')) {
     const postTitle = title.split(' | ')[0] || 'A post'
 
-    presenceData.details = 'Reading a post'
-    presenceData.state = postTitle
+    presenceData = {
+      type: ActivityType.Watching,
+      largeImageKey: 'logo',
+      largeImageText: 'usehangar.gg',
+      startTimestamp,
+      details: 'Reading a post',
+      state: postTitle
+    }
   }
 
   presence.setActivity(presenceData)
