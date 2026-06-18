a103873f7e790ba31f2a13662d406e6ef3583483
zzxncl <niyarey31@gmail.com>
2026-06-18 04:09:05 -0400

    Implement presence functionality for kitsu.live
    Signed-off-by: zzxncl <niyarey31@gmail.com>

---
+++ websites/K/kitsu.live/presence.ts
@@ -0,0 +1,37 @@
+const presence = new Presence({ clientId: '1515251988646989854' })
+
+presence.on('UpdateData', () => {
+  const el = document.getElementById('kitsu-presence-data')
+
+  if (!el || !el.getAttribute('data-active')) {
+    presence.setActivity()
+    return
+  }
+
+  const details = el.getAttribute('data-details') || undefined
+  const state = el.getAttribute('data-state') || undefined
+  const largeImageKey = el.getAttribute('data-large-image') || undefined
+  const largeImageText = el.getAttribute('data-large-image-text') || undefined
+  const startTimestampStr = el.getAttribute('data-start-timestamp')
+  const endTimestampStr = el.getAttribute('data-end-timestamp')
+
+  const startTimestamp = startTimestampStr ? parseInt(startTimestampStr, 10) * 1000 : undefined
+  const endTimestamp = endTimestampStr ? parseInt(endTimestampStr, 10) * 1000 : undefined
+
+  presence.setActivity({
+    details,
+    state,
+    largeImageKey,
+    largeImageText,
+    smallImageKey: 'kitsu_logo',
+    smallImageText: 'kitsu.live',
+    startTimestamp,
+    endTimestamp,
+    buttons: [
+      {
+        label: 'Watch on kitsu.live',
+        url: document.location.href,
+      },
+    ],
+  })
+})
