8c0b072cb15d9e29297ed9da4687a6ea1659c0a8
vituhas <vitusousa22@gmail.com>
2026-06-27 16:57:35 -0300

    Refactor presence details to use 'p.getString()'
    Signed-off-by: vituhas <vitusousa22@gmail.com>

--- websites/B/Basketball Reference/presence.ts
+++ websites/B/Basketball Reference/presence.ts
@@ -11,46 +11,47 @@ presence.on('UpdateData', async () => {
   }
 
   const { pathname } = document.location
+  const p = presence as any
 
   if (/^\/players\/[a-z]\/\w+\.html/.test(pathname)) {
     const playerName
       = (document.querySelector('h1[itemprop=\'name\']')?.textContent || '').trim()
         || (document.title.split(' Stats')[0] || '').trim()
-    presenceData.details = presence.i18n.getString('viewingPlayer')
+    presenceData.details = p.getString('viewingPlayer')
     presenceData.state = playerName
   }
   else if (/^\/teams\/[A-Z]+\/\d+\.html/.test(pathname)) {
     const teamName
       = (document.querySelector('h1')?.textContent || '').trim()
         || (document.title.split(' Roster')[0] || '').trim()
-    presenceData.details = presence.i18n.getString('viewingTeam')
+    presenceData.details = p.getString('viewingTeam')
     presenceData.state = teamName
   }
   else if (/^\/leagues\/NBA_\d+\.html/.test(pathname)) {
     const season = pathname.match(/NBA_(\d+)/)?.[1] || ''
-    presenceData.details = presence.i18n.getString('browsingSeason')
+    presenceData.details = p.getString('browsingSeason')
     presenceData.state = `NBA ${season}`
   }
   else if (pathname.startsWith('/boxscores/')) {
-    presenceData.details = presence.i18n.getString('viewingBoxScore')
+    presenceData.details = p.getString('viewingBoxScore')
     presenceData.state = document.title.replace(' Box Score', '').trim()
   }
   else if (pathname.startsWith('/leaders/')) {
-    presenceData.details = presence.i18n.getString('browsingLeaders')
+    presenceData.details = p.getString('browsingLeaders')
     presenceData.state = 'NBA Leaders'
   }
   else if (pathname.startsWith('/playoffs/')) {
     const year = pathname.match(/\d{4}/)?.[0] || ''
-    presenceData.details = presence.i18n.getString('browsingPlayoffs')
+    presenceData.details = p.getString('browsingPlayoffs')
     presenceData.state = `NBA Playoffs ${year}`
   }
   else if (pathname.includes('/search/')) {
     const query = new URLSearchParams(document.location.search).get('search') || ''
-    presenceData.details = presence.i18n.getString('searching')
+    presenceData.details = p.getString('searching')
     presenceData.state = `"${query}"`
   }
   else {
-    presenceData.details = presence.i18n.getString('browsingHome')
+    presenceData.details = p.getString('browsingHome')
   }
 
   if (presenceData.details)
