308cc2b990933bff105e743723b0e28317d0b981
vituhas <vitusousa22@gmail.com>
2026-07-05 13:11:43 -0300

    Add presence.js
    Signed-off-by: vituhas <vitusousa22@gmail.com>

---
+++ websites/W/WorldGuessr/presence.js
@@ -0,0 +1,41 @@
+const presence = new Presence({
+  clientId: '1523354893841072229',
+})
+
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+presence.on('UpdateData', async () => {
+  const strings = await presence.getStrings({
+    playingSingleplayer: 'worldguessr.playingSingleplayer',
+    playingMultiplayer: 'worldguessr.playingMultiplayer',
+    browsingHome: 'general.viewHome',
+  })
+
+  const presenceData = {
+    largeImageKey: 'https://i.imgur.com/s6GY1qt.png',
+    startTimestamp: browsingTimestamp,
+  }
+
+  const { pathname } = document.location
+
+  if (pathname === '/' || pathname === '/index.html') {
+    presenceData.details = strings.browsingHome
+  } 
+  else if (pathname.startsWith('/room/')) {
+    const roomId = pathname.split('/room/')[1] || ''
+    presenceData.details = strings.playingMultiplayer
+    presenceData.state = `Sala: ${roomId}`
+  } 
+  else if (pathname.startsWith('/game') || pathname.startsWith('/play')) {
+    presenceData.details = strings.playingSingleplayer
+  } 
+  else {
+    presenceData.details = strings.browsingHome
+  }
+
+  if (presenceData.details) {
+    presence.setActivity(presenceData)
+  } else {
+    presence.clearActivity()
+  }
+})
