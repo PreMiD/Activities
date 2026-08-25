3232839c5fffd107031a35580686c08b9782290a
Demian Medve <132155033+NoodlesNom@users.noreply.github.com>
2026-08-24 23:38:49 -0500

    Fix ESLint issues
    fixed formatting
    
    Signed-off-by: Demian Medve <132155033+NoodlesNom@users.noreply.github.com>

--- websites/A/Amazon Music/presence.ts
+++ websites/A/Amazon Music/presence.ts
@@ -85,16 +85,8 @@ presence.on('UpdateData', async () => {
     presenceData.smallImageKey = paused ? Assets.Pause : Assets.Play
     presenceData.smallImageText = paused ? strings.pause : strings.play
 
-    presenceData.startTimestamp = Math.floor(Date.now() / 1000) - timestampFromFormat(document
-          .querySelector('div.sXaGQzYs9WqImj2uxDCBs > span:nth-child(1)')!
-          .textContent!
-          .match(/[0-9:]+/)![0],
-    )
-    presenceData.endTimestamp = Math.floor(Date.now() / 1000) + timestampFromFormat(document
-          .querySelector('div.sXaGQzYs9WqImj2uxDCBs > span:nth-child(2)')!
-          .textContent!
-          .match(/[0-9:]+/)![0],
-      )
+    presenceData.startTimestamp = Math.floor(Date.now() / 1000) - timestampFromFormat(document.querySelector('div.sXaGQzYs9WqImj2uxDCBs > span:nth-child(1)')!.textContent!.match(/[0-9:]+/)![0],)
+    presenceData.endTimestamp = Math.floor(Date.now() / 1000) + timestampFromFormat(document.querySelector('div.sXaGQzYs9WqImj2uxDCBs > span:nth-child(2)')!.textContent!.match(/[0-9:]+/)![0],)
 
     if (cover) {
       presenceData.largeImageKey = navigator.mediaSession.metadata.artwork[0]?.src
