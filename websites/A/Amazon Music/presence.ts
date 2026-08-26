e60f65a0a7531d446cf391cad7ec679c0e36c0cd
Demian Medve <132155033+NoodlesNom@users.noreply.github.com>
2026-08-25 19:32:31 -0500

    fix pause/play strings
    Co-authored-by: Skully <117316808+skullysmods@users.noreply.github.com>
    Signed-off-by: Demian Medve <132155033+NoodlesNom@users.noreply.github.com>

--- websites/A/Amazon Music/presence.ts
+++ websites/A/Amazon Music/presence.ts
@@ -7,8 +7,8 @@ const presence = new Presence({
 async function getStrings() {
   return presence.getStrings(
     {
-      play: 'Playing',
-      pause: 'Paused',
+      play: 'general.playing',
+      pause: 'general.paused',
       viewPlaylist: 'general.buttonViewPlaylist',
       viewArtist: 'general.buttonViewArtist',
     },
