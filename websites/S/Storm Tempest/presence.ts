7ae74f211f68e733654277604e920a093509b9c7
Mitsuki9 <akuyaki.rest@gmail.com>
2026-09-16 23:25:09 +0300

    yy

--- websites/S/Storm Tempest/presence.ts
+++ websites/S/Storm Tempest/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 })
 
 const STORM_TEMPEST_IMAGE
-  = 'https://raw.githubusercontent.com/akuyakii/assist-/main/logo-512.png'
+  = 'https://akuyaki.rest/logo-512.png'
 
 interface WatchState {
   title: string
