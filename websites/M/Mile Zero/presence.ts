5374838b043dc45a999377f6b0b0d1602c9d0415
richie <richardvanbogaert@gmail.com>
2026-09-16 22:19:12 +0100

    feat(Mile Zero): add activity

--- websites/M/Mile Zero/presence.ts
+++ websites/M/Mile Zero/presence.ts
@@ -126,7 +126,7 @@ updateNowPlaying()
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    largeImageKey: 'https://i.richie.media/u/HzpecO.png',
+    largeImageKey: 'https://i.richie.media/u/6qR5hR.png',
   }
 
   const playButton = document.querySelector<HTMLButtonElement>('.hero-card__play')
