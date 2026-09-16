7372d7e621b32d1567d8414a9c377953b3452e6f
Mitsuki9 <akuyaki.rest@gmail.com>
2026-09-16 18:43:35 +0300

    dd

--- websites/S/Storm Tempest/presence.ts
+++ websites/S/Storm Tempest/presence.ts
@@ -248,7 +248,7 @@ function getVideo(): HTMLVideoElement | undefined {
 
 function isWatchPage(): boolean {
   const url
-  = `${window.location.pathname}${window.location.search}`.toLowerCase()
+      = `${window.location.pathname}${window.location.search}`.toLowerCase()
 
   return (
     /\/watch\b/.test(url)
